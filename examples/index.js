c0c2a1894c0e1673bd0f14d1353ff4904706728d
wanlei <wanlei@yidian-inc.com>
2017-11-02 19:40:49 +0800

    update: [Tooltip] unsolved animation & transition

--- examples/index.js
+++ examples/index.js
@@ -3,13 +3,15 @@ import Checkbox from './checkbox.vue'
 import Button from './button.vue'
 import Radio from './radio.vue'
 import Grid from './grid.vue'
+import ToolTip from './tooltip.vue'
 // import Dialog from './dialog.vue'
 import './index.less'
-new Vue({
+window.root = new Vue({
   el: '#app',
   template: `
     <div>
       <Grid />
+      <ToolTip />
       <Checkbox />
       <AntButton />
       <Radio />
@@ -21,5 +23,6 @@ new Vue({
     Checkbox,
     Grid,
     Radio,
+    ToolTip,
   },
 })

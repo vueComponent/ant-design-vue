0651911ce70a314ce670d958366b011f04c5fdad
wanlei <wanlei@yidian-inc.com>
2017-11-03 15:35:25 +0800

    update: [Tooltip] receive updates

--- examples/tooltip.vue
+++ examples/tooltip.vue
@@ -1,9 +1,11 @@
 <template>
-  <tool-tip
-    placement="top"
-    title="nihaoaaaaaaaaaaaaaaaaaaaaa">
-    <h1 @click="boom" style="display: inline-block">This is just a test, put your cursor here</h1>
-  </tool-tip>
+  <div>
+    <tool-tip
+      placement="top"
+      :title="showText">
+      <h1 @click="boom" style="display: inline-block">This is just a test, put your cursor here</h1>
+    </tool-tip>
+  </div>
 </template>
 
 <script>
@@ -11,11 +13,14 @@
 	export default {
 		name: '',
 		data() {
-			return {}
+			return {
+        show: true,
+        showText: '你好啊，23'
+      }
 		},
     methods: {
 		  boom() {
-		    console.info(23333)
+		    this.showText += '3'
       }
     },
     components: {
@@ -23,7 +28,3 @@
     }
 	}
 </script>
-
-<style scoped>
-
-</style>

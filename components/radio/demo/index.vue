284126e91d13b4c4cc87692c70f44d93033a57f5
tangjinzhou <tangjinzhou@yidian-inc.com>
2017-11-07 12:04:50 +0800

    add radio demo

--- components/radio/demo/index.vue
+++ components/radio/demo/index.vue
@@ -4,16 +4,39 @@
     <Basic />
     <h1>Disabled</h1>
     <Disabled />
-    <Layout />
+    <h1>RadioButton</h1>
+    <RadioButton />
+    <h1>RadioGroupMore</h1>
+    <RadioGroupMore />
+    <h1>RadioGroupOptions</h1>
+    <RadioGroupOptions />
+    <h1>RadioGroupWithName</h1>
+    <RadioGroupWithName />
+    <h1>RadioGroup</h1>
+    <RadioGroup />
+    <h1>Size</h1>
+    <Size />
   </div>
 </template>
 <script>
 import Basic from './basic'
 import Disabled from './disabled'
+import RadioButton from './radioButton'
+import RadioGroupMore from './radioGroup-more'
+import RadioGroupOptions from './radioGroup-options'
+import RadioGroupWithName from './radioGroup-with-name'
+import RadioGroup from './radioGroup'
+import Size from './size'
 export default {
   components: {
     Basic,
     Disabled,
+    RadioButton,
+    RadioGroupMore,
+    RadioGroupOptions,
+    RadioGroupWithName,
+    RadioGroup,
+    Size,
   },
 }
 </script>

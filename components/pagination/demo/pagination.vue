b838be1d3ee125bba58c2c213c73de806e305519
wangxueliang <sandianbaozi@163.com>
2017-11-07 14:16:17 +0800

    pagination showTotal

--- components/pagination/demo/pagination.vue
+++ components/pagination/demo/pagination.vue
@@ -8,7 +8,7 @@
       <vc-button @click="changeValue">改值</vc-button>
     </div>
     <div>
-      <pagination v-model="current" :total="total"></pagination>
+      <pagination v-model="current" :total="total" :showTotal="showTotal"></pagination>
       <vc-button @click="changeValue">改值</vc-button>
       <vc-button @click="getValue">当前值</vc-button>
     </div>
@@ -22,7 +22,7 @@ export default {
     return {
       simple: true,
       current: 1,
-      total: 500,
+      total: 483,
     }
   },
   methods: {
@@ -31,6 +31,9 @@ export default {
     },
     getValue () {
       alert(this.current)
+    },
+    showTotal (total) {
+      return `共 ${total} 条`
     },
   },
   components: {

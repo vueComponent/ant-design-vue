b0a21a5b08850fd4a774e243537b6fc593da9075
wangxueliang <sandianbaozi@163.com>
2017-11-16 18:29:02 +0800

    badge init for  v1.0

--- components/avatar/demo/index.vue
+++ components/avatar/demo/index.vue
@@ -5,11 +5,13 @@
       <avatar icon="user"/>
       <avatar size="small" icon="user"/>
     </div>
+    <br/>
     <div>
       <avatar shape="square" size="large" icon="user" />
       <avatar shape="square" icon="user" />
       <avatar shape="square" size="small" icon="user" />
     </div>
+    <br/>
     <div>
       <avatar icon="user" />
       <avatar>U</avatar>
@@ -18,15 +20,22 @@
       <avatar :styles="{ color: '#f56a00', backgroundColor: '#fde3cf' }">U</avatar>
       <avatar :styles="{ backgroundColor: '#87d068' }" icon="user" />
     </div>
+    <br/>
     <div>
       <avatar shape="square" size="large">{{avatarValue}}</avatar>
       <AntButton @click="changeValue">改变</AntButton>
+    </div>
+    <br/>
+    <div>
+      <Badge count=1><Avatar shape="square" icon="user" /></Badge>
+      <br/>
+      <Badge dot><Avatar shape="square" icon="user" /></Badge>
     </div>
   </div>
 </template>
 <script>
 import '../style'
-import { Avatar, Button } from 'antd/index'
+import { Avatar, Button, Badge } from 'antd/index'
 export default {
   data () {
     return {
@@ -41,6 +50,7 @@ export default {
   components: {
     Avatar,
     AntButton: Button,
+    Badge,
   },
 }
 </script>

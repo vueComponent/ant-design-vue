1bbfccae19629519277114a92f21e60de9d2ab11
wangxueliang <wangxueliang@yidian-inc.com>
2018-01-15 10:52:16 +0800

    breadcrumb

---
+++ components/breadcrumb/BreadcrumbItem.vue
@@ -0,0 +1,33 @@
+<script>
+  import PropTypes from '../_util/vue-types'
+
+  export default {
+    name: 'BreadcrumbItem',
+    inject: ['breadCrumbParent'],
+    props: {
+      prefixCls: PropTypes.string.def('ant-breadcrumb'),
+      href: PropTypes.string,
+    },
+    render () {
+      const { prefixCls, href, ...restProps } = this
+      const { breadCrumbParent = {}} = this
+      const { separator = '/' } = breadCrumbParent
+      const solt = this.$slots.default
+      let link
+      if (href !== undefined) {
+        link = <a class={`${prefixCls}-link`} {...restProps}>{solt}</a>
+      } else {
+        link = <span class={`${prefixCls}-link`} {...restProps}>{solt}</span>
+      }
+      if (solt) {
+        return (
+          <span>
+            {link}
+            <span class={`${prefixCls}-separator`}>{separator}</span>
+          </span>
+        )
+      }
+      return null
+    },
+  }
+</script>
---
+++ components/breadcrumb/demo/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    基本用法
+    <Breadcrumb>
+      <BreadcrumbItem>Home</BreadcrumbItem>
+      <BreadcrumbItem><a href="">Application Center</a></BreadcrumbItem>
+      <BreadcrumbItem><a href="">Application List</a></BreadcrumbItem>
+      <BreadcrumbItem>An Application</BreadcrumbItem>
+    </Breadcrumb>
+    <br>
+    带有图标
+    <Breadcrumb>
+      <BreadcrumbItem href="">
+        <Icon type="home" />
+      </BreadcrumbItem>
+      <BreadcrumbItem href="">
+        <Icon type="user" />
+        <span>Application List</span>
+      </BreadcrumbItem>
+      <BreadcrumbItem>
+        Application
+      </BreadcrumbItem>
+    </Breadcrumb>
+    <br>
+    自定义分隔符
+    <Breadcrumb separator=">">
+      <BreadcrumbItem>Home</BreadcrumbItem>
+      <BreadcrumbItem href="">Application Center</BreadcrumbItem>
+      <BreadcrumbItem href="">Application List</BreadcrumbItem>
+      <BreadcrumbItem>An Application</BreadcrumbItem>
+    </Breadcrumb>
+  </div>
+</template>
+<script>
+import '../style'
+import { Icon, Breadcrumb } from 'antd/index'
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+  },
+  components: {
+    Icon,
+    Breadcrumb,
+    BreadcrumbItem: Breadcrumb.Item,
+  },
+}
+</script>

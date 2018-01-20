7f083cb99c39ddf06c156eb0642c824c881d2795
tjz <415800467@qq.com>
2018-01-20 21:04:31 +0800

    fix

--- examples/routes.js
+++ examples/routes.js
@@ -1,6 +1,6 @@
 const AsyncComp = () => {
   const pathnameArr = window.location.pathname.split('/')
-  const com = pathnameArr[1] || 'button'
+    const com = pathnameArr[1] || 'button' // eslint-disable-line
   const demo = pathnameArr[2] || 'index'
   return {
     component: import(`../components/card/demo/${demo}.vue`),

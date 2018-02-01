9f6e4b11e8d645803a2878d0f133637f40430897
tangjinzhou <415800467@qq.com>
2018-02-01 14:23:59 +0800

    fix

--- examples/components/demo.vue
+++ examples/components/demo.vue
@@ -1,6 +1,7 @@
 <script>
 import { Menu } from 'antd'
 import * as AllDemo from '../demo'
+import Header from './header'
 const MenuItem = Menu.Item
 export default {
   render () {
@@ -11,14 +12,17 @@ export default {
       lang = 'us'
     }
     return (
-      <div class='site page-container'>
-        <Menu class='nav' selectedKeys={[name]}>
-          {Object.keys(AllDemo).map(d => <MenuItem key={d}>
-            <router-link to={{ path: `/components/${lang}/${d}` }}>{d}</router-link>
-          </MenuItem>)}
-        </Menu>
-        <div class='content main-container'>
-          {Demo ? <Demo /> : '正在紧急开发中...'}
+      <div class='site'>
+        <Header />
+        <div class='main-wrapper'>
+          <Menu class='nav' selectedKeys={[name]}>
+            {Object.keys(AllDemo).map(d => <MenuItem key={d}>
+              <router-link to={{ path: `/components/${lang}/${d}` }}>{d}</router-link>
+            </MenuItem>)}
+          </Menu>
+          <div class='content main-container'>
+            {Demo ? <Demo /> : '正在紧急开发中...'}
+          </div>
         </div>
 
       </div>

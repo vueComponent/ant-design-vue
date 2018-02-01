3035b83e15e1cd2a104834dcd454bd6835d86e4a
tangjinzhou <415800467@qq.com>
2018-02-01 14:23:59 +0800

    fix

---
+++ examples/components/header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header id='header'>
+    <a-menu v-model="current" mode="horizontal" id="nav">
+      <a-menu-item key='home'>
+        首页
+      </a-menu-item>
+      <a-menu-item key='design'>
+        设计语言
+      </a-menu-item>
+      <a-menu-item key='components'>
+        组件
+      </a-menu-item>
+    </a-menu>
+  </header>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      current: ['components'],
+    }
+  },
+}
+</script>

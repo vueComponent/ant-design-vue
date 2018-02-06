02b00f0c4e579839f3677ba67da8dd4c236d9c72
tangjinzhou <415800467@qq.com>
2018-02-06 19:00:34 +0800

    add vc-select

---
+++ components/vc-select/OptGroup.vue
@@ -0,0 +1,5 @@
+<script>
+export default {
+  isSelectOptGroup: true,
+}
+</script>
---
+++ components/vc-select/Option.vue
@@ -0,0 +1,13 @@
+<script>
+import PropTypes from '../_util/vue-types'
+
+export default {
+  props: {
+    value: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number,
+    ]),
+  },
+  isSelectOption: true,
+}
+</script>

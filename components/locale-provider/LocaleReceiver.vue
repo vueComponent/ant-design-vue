badc519821e45a9d3178dd38d1e6fd664c32adb4
tangjinzhou <415800467@qq.com>
2018-02-23 16:14:59 +0800

    add 国际化

---
+++ components/locale-provider/LocaleReceiver.vue
@@ -0,0 +1,43 @@
+<script>
+import PropTypes from '../_util/vue-types'
+
+export default {
+  props: {
+    componentName: PropTypes.string,
+    defaultLocale: PropTypes.oneOfType([
+      PropTypes.object,
+      PropTypes.func,
+    ]),
+    children: PropTypes.func,
+  },
+  inject: {
+    antLocale: { default: {}},
+  },
+  methods: {
+    getLocale () {
+      const { componentName, defaultLocale } = this.props
+      const { antLocale } = this
+      const localeFromContext = antLocale && antLocale[componentName]
+      return {
+        ...(typeof defaultLocale === 'function' ? defaultLocale() : defaultLocale),
+        ...(localeFromContext || {}),
+      }
+    },
+
+    getLocaleCode () {
+      const { antLocale } = this
+      const localeCode = antLocale && antLocale.locale
+      // Had use LocaleProvide but didn't set locale
+      if (antLocale && antLocale.exist && !localeCode) {
+        return 'en-us'
+      }
+      return localeCode
+    },
+  },
+
+  render () {
+    return this.children(this.getLocale(), this.getLocaleCode())
+  },
+}
+
+</script>

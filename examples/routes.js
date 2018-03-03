6e8b5b9a2021c81367b60326e03194ee99eb0a96
tjz <415800467@qq.com>
2018-03-03 19:14:03 +0800

    add cascader

--- examples/routes.js
+++ examples/routes.js
@@ -3,7 +3,7 @@ const AsyncComp = () => {
   const hashs = window.location.hash.split('/')
   const d = hashs[hashs.length - 1]
   return {
-    component: import(`../components/vc-cascader/demo/${d}`),
+    component: import(`../components/cascader/demo/${d}`),
   }
 }
 export default [

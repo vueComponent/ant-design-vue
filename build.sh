0d3ff16090526b34a53cd5c4f62746f74d67c1b0
tangjinzhou <415800467@qq.com>
2018-03-19 10:27:03 +0800

    fix fime name

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 rm -rf dist
 mkdir dist
-./node_modules/.bin/webpack --config webpack.prod.config.js
+./node_modules/.bin/webpack --config webpack.site.config.js
 cp dist/index.html index.html

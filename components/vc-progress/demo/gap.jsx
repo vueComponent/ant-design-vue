045f95201df387b42e1b3eb515ced296ae0ece69
wangxueliang <wangxueliang@yidian-inc.com>
2018-04-03 11:00:05 +0800

    add vc-progress

---
+++ components/vc-progress/demo/gap.jsx
@@ -0,0 +1,72 @@
+import { Circle } from '../index'
+import '../assets/index.less'
+
+export default {
+  data () {
+    return {
+      percent: 30,
+      color: '#3FC7FA',
+    }
+  },
+  methods: {
+    changeState () {
+      const colorMap = ['#3FC7FA', '#85D262', '#FE8C6A']
+      const value = parseInt(Math.random() * 100, 10)
+      this.percent = value
+      this.color = colorMap[parseInt(Math.random() * 3, 10)]
+    },
+  },
+  render () {
+    const circleContainerStyle = {
+      width: '200px',
+      height: '200px',
+    }
+    return (
+      <div>
+        <div style={circleContainerStyle}>
+          <Circle
+            percent={this.percent}
+            gapDegree='70'
+            gapPosition='top'
+            strokeWidth='6'
+            strokeLinecap='square'
+            strokeColor={this.color}
+          />
+        </div>
+        <div style={circleContainerStyle}>
+          <Circle
+            percent={this.percent}
+            gapDegree='70'
+            gapPosition='bottom'
+            strokeWidth='6'
+            strokeLinecap='square'
+            strokeColor={this.color}
+          />
+        </div>
+        <div style={circleContainerStyle}>
+          <Circle
+            percent={this.percent}
+            gapDegree='70'
+            gapPosition='left'
+            strokeWidth='6'
+            strokeLinecap='square'
+            strokeColor={this.color}
+          />
+        </div>
+        <div style={circleContainerStyle}>
+          <Circle
+            percent={this.percent}
+            gapDegree='70'
+            gapPosition='right'
+            strokeWidth='6'
+            strokeLinecap='square'
+            strokeColor={this.color}
+          />
+        </div>
+        <p>
+          <button onClick={this.changeState}>Change State</button>
+        </p>
+      </div>
+    )
+  },
+}
---
+++ components/vc-progress/demo/simple.jsx
@@ -0,0 +1,49 @@
+import { Line, Circle } from '../index'
+import '../assets/index.less'
+
+export default {
+  data () {
+    return {
+      percent: 30,
+      color: '#3FC7FA',
+    }
+  },
+  methods: {
+    changeState () {
+      const colorMap = ['#3FC7FA', '#85D262', '#FE8C6A']
+      const value = parseInt(Math.random() * 100, 10)
+      this.percent = value
+      this.color = colorMap[parseInt(Math.random() * 3, 10)]
+    },
+  },
+  render () {
+    const containerStyle = {
+      width: '250px',
+    }
+    const circleContainerStyle = {
+      width: '250px',
+      height: '250px',
+      display: 'inline-block',
+    }
+    return (
+      <div>
+        <h3>Line Progress {this.percent}%</h3>
+        <div style={containerStyle}>
+          <Line percent={this.percent} strokeWidth='4' strokeColor={this.color} />
+        </div>
+        <h3>Circle Progress {this.percent}%</h3>
+        <div style={circleContainerStyle}>
+          <Circle
+            percent={this.percent}
+            strokeWidth='6'
+            strokeLinecap='round'
+            strokeColor={this.color}
+          />
+        </div>
+        <p>
+          <button onClick={this.changeState}>Change State</button>
+        </p>
+      </div>
+    )
+  },
+}

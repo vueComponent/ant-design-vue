aaaa776ba0fa0a450fc055d6b4c23394cc68610d
wangxueliang <wangxueliang@yidian-inc.com>
2018-04-04 13:57:23 +0800

    add inputNumber

--- examples/routes.js
+++ examples/routes.js
@@ -3,7 +3,7 @@ const AsyncComp = () => {
   const hashs = window.location.hash.split('/')
   const d = hashs[hashs.length - 1]
   return {
-    component: import(`../components/vc-input-number/demo/${d}`),
+    component: import(`../components/input-number/demo/${d}`),
   }
 }
 export default [

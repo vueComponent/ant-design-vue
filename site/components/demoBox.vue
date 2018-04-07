859dc19b06a4fe84fd5c114c023816f2e88a5064
tjz <415800467@qq.com>
2018-04-07 15:22:47 +0800

    fix site

--- site/components/demoBox.vue
+++ site/components/demoBox.vue
@@ -53,11 +53,12 @@ export default {
         `not have usTitle`,
       )
     }
-    let id = ['components', name.replace('-cn', ''), 'demo', ...usTitle.split(' ')].join('-').toLowerCase()
-
+    const component = this.$slots.component[0]
+    const fileName = (component.context.$vnode.componentOptions.Ctor.extendOptions.__file.split('/demo/')[1] || '').replace('.md', '')
+    const id = ['components', name.replace('-cn', ''), 'demo', fileName].join('-').toLowerCase()
+    console.log(fileName)
     if (this._store.store) {
       const { currentSubMenu } = this._store.store.getState()
-      id = `${id}-${currentSubMenu.length + 1}`
       this._store.store.setState({ currentSubMenu: [...currentSubMenu, { cnTitle, usTitle, id }] })
     }
     return {

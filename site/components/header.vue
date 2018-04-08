d918a1249e8a30f7c37d3ba6e907b86804d92d22
tjz <415800467@qq.com>
2018-04-08 21:23:50 +0800

    fix

--- site/components/header.vue
+++ site/components/header.vue
@@ -25,7 +25,7 @@ export default {
         <a-row>
           <a-col xxl={4} xl={5} lg={5} md={6} sm={24} xs={24}>
             <router-link to={{ path: '/ant-design' }} id='logo'>
-              <img alt='logo' src='https://raw.githubusercontent.com/vueComponent/ant-design/master/logo.png' />
+              <img alt='logo' style="height: 32px" src='https://raw.githubusercontent.com/vueComponent/ant-design/master/logo.png' />
               <span>  VUE-ANTD</span>
             </router-link>
           </a-col>

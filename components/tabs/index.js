c1cd8f46f082eda560734a109a2bf7eb78aa1975
tjz <415800467@qq.com>
2018-04-11 21:58:52 +0800

    fix

--- components/tabs/index.js
+++ components/tabs/index.js
@@ -1,7 +1,7 @@
 import Tabs from './tabs'
 import TabPane from '../vc-tabs/src/TabPane'
 import TabContent from '../vc-tabs/src/TabContent'
-Tabs.TabPane = { ...TabPane, name: 'ATabPane' }
+Tabs.TabPane = { ...TabPane, name: 'ATabPane', __ANT_TAB_PANE: true }
 Tabs.TabContent = { ...TabContent, name: 'ATabContent' }
 export default Tabs
 export { TabPane, TabContent }

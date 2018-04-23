5f9e8cc59d933423d30628067c3375be93a1a37a
tangjinzhou <415800467@qq.com>
2018-04-23 18:46:11 +0800

    test: update button test

--- components/button/__test__/index.spec.js
+++ components/button/__test__/index.spec.js
@@ -1,18 +1,56 @@
 import Button from '../index'
-import {mount} from 'avoriaz'
+import Icon from '../../icon'
+import { mount } from 'avoriaz'
+import Vue from 'vue'
 
 describe('Button', () => {
-  let wrapper
-
-  afterEach(() => {
-    wrapper && wrapper.destroy()
-  })
   it('create primary button', () => {
-    wrapper = mount(Button, {
-      propsData: {
-        type: 'primary',
+    const wrapper = mount({
+      render (h) {
+        return <Button type='primary'>按钮</Button>
       },
     })
     expect(wrapper.hasClass('ant-btn-primary')).to.equal(true)
   })
+  it('renders Chinese characters correctly', (done) => {
+    const wrapper = mount(
+      {
+        render (h) {
+          return <Button>按钮</Button>
+        },
+      }
+    )
+    expect(wrapper.text()).to.equal('按 钮')
+
+    const wrapper1 = mount(
+      {
+        render (h) {
+          return <Button icon='search'>按钮</Button>
+        },
+      }
+    )
+    expect(wrapper1.text()).to.equal('按钮')
+
+    const wrapper2 = mount(
+      {
+        render (h) {
+          return <Button><Icon type="search" />按钮</Button>
+        },
+      }
+    )
+    expect(wrapper2.text()).to.equal('按钮')
+
+    const wrapper3 = mount(
+      {
+        render (h) {
+          return <Button><span>按钮</span></Button>
+        },
+      }
+    )
+    Vue.nextTick(() => {
+      expect(wrapper3.find('.ant-btn')[0].hasClass('ant-btn-two-chinese-chars')).to.equal(true);
+      done()
+    })
+  })
+
 })

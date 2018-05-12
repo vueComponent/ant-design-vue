72fde617a39d733dc8885130a47cc2ba44aec04c
tangjinzhou <415800467@qq.com>
2018-05-12 13:08:35 +0800

    test: open autowatch

--- test/index.js
+++ test/index.js
@@ -10,5 +10,5 @@ testsContext.keys().forEach(testsContext)
 // require all src files except main.js for coverage.
 // you can also change this to match only the subset of files that
 // you want coverage for.
-const srcContext = require.context(`../components`, true, /^\.(\.js|\.jsx)?$/)
+const srcContext = require.context(`../components/${process.env.SCOPE}`, true, /^\.(\.js|\.jsx)?$/)
 srcContext.keys().forEach(srcContext)

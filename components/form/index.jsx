86a8c76bfbd2d4b7204c9e3cb4f4a494f8662b4a
tangjinzhou <415800467@qq.com>
2018-05-15 10:08:14 +0800

    fix: form ant-ref directive bug(#20)

--- components/form/index.jsx
+++ components/form/index.jsx
@@ -1,4 +1,8 @@
+import Vue from 'vue'
 import Form from './Form'
+import antRefDirective from '../_util/antRefDirective'
+
+Vue.use(antRefDirective)
 
 export { FormProps, FormComponentProps, FormCreateOption, ValidateCallback, ValidationRule } from './Form'
 export { FormItemProps } from './FormItem'

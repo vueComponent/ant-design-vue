07150c09653a4f72f284696c93f673dacb8e29f8
tjz <415800467@qq.com>
2018-05-26 22:48:16 +0800

    test: add table test

---
+++ components/table/__tests__/Table.pagination.test.js
@@ -0,0 +1,217 @@
+
+import { mount } from '@vue/test-utils'
+import Table from '..'
+import Vue from 'vue'
+
+describe('Table.pagination', () => {
+  const columns = [{
+    title: 'Name',
+    dataIndex: 'name',
+  }]
+
+  const data = [
+    { key: 0, name: 'Jack' },
+    { key: 1, name: 'Lucy' },
+    { key: 2, name: 'Tom' },
+    { key: 3, name: 'Jerry' },
+  ]
+
+  const pagination = { class: 'my-page', pageSize: 2 }
+
+  function getTableOptions (props = {}, listeners = {}) {
+    return {
+      propsData: {
+        columns,
+        dataSource: data,
+        pagination,
+        ...props,
+      },
+      listeners: {
+        ...listeners,
+      },
+      sync: false,
+    }
+    // return {
+    //   render () {
+    //     return (
+    //       <Table
+    //         {...tableProps}
+    //       />
+    //     )
+    //   },
+    // }
+  }
+
+  function renderedNames (wrapper) {
+    return wrapper.findAll({ name: 'TableRow' }).wrappers.map(row => {
+      return row.props().record.name
+    })
+  }
+
+  it('renders pagination correctly', (done) => {
+    const wrapper = mount(Table, getTableOptions())
+    Vue.nextTick(() => {
+      expect(wrapper.html()).toMatchSnapshot()
+      done()
+    })
+  })
+
+  it('should not show pager if pagination.hideOnSinglePage is true and only 1 page', (done) => {
+    const wrapper = mount(Table, getTableOptions({ pagination: { pageSize: 3, hideOnSinglePage: true }}))
+    Vue.nextTick(() => {
+      expect(wrapper.findAll('.ant-pagination')).toHaveLength(1)
+      wrapper.setProps({ pagination: { pageSize: 3, hideOnSinglePage: false }})
+      Vue.nextTick(() => {
+        expect(wrapper.findAll('.ant-pagination')).toHaveLength(1)
+        wrapper.setProps({ pagination: { pageSize: 4, hideOnSinglePage: true }})
+        Vue.nextTick(() => {
+          expect(wrapper.findAll('.ant-pagination')).toHaveLength(0)
+          wrapper.setProps({ pagination: { pageSize: 4, hideOnSinglePage: false }})
+          Vue.nextTick(() => {
+            expect(wrapper.findAll('.ant-pagination')).toHaveLength(1)
+            wrapper.setProps({ pagination: { pageSize: 5, hideOnSinglePage: true }})
+            Vue.nextTick(() => {
+              expect(wrapper.findAll('.ant-pagination')).toHaveLength(0)
+              wrapper.setProps({ pagination: { pageSize: 5, hideOnSinglePage: false }})
+              Vue.nextTick(() => {
+                expect(wrapper.findAll('.ant-pagination')).toHaveLength(1)
+                done()
+              })
+            })
+          })
+        })
+      })
+    })
+  })
+
+  it('paginate data', (done) => {
+    const wrapper = mount(Table, getTableOptions())
+    Vue.nextTick(() => {
+      expect(renderedNames(wrapper)).toEqual(['Jack', 'Lucy'])
+      const pager = wrapper.findAll({ name: 'Pager' })
+      pager.at(pager.length - 1).trigger('click')
+      Vue.nextTick(() => {
+        expect(renderedNames(wrapper)).toEqual(['Tom', 'Jerry'])
+        done()
+      })
+    })
+  })
+
+  it('repaginates when pageSize change', () => {
+    const wrapper = mount(Table, getTableOptions())
+    wrapper.setProps({ pagination: { pageSize: 1 }})
+    Vue.nextTick(() => {
+      expect(renderedNames(wrapper)).toEqual(['Jack'])
+    })
+  })
+
+  it('fires change event', (done) => {
+    const handleChange = jest.fn()
+    const handlePaginationChange = jest.fn()
+    const noop = () => {}
+    const wrapper = mount(Table, getTableOptions({
+      pagination: { ...pagination, onChange: handlePaginationChange, onShowSizeChange: noop },
+
+    }, { change: handleChange }))
+    Vue.nextTick(() => {
+      const pager = wrapper.findAll({ name: 'Pager' })
+      pager.at(pager.length - 1).trigger('click')
+
+      expect(handleChange).toBeCalledWith(
+        {
+          class: 'my-page',
+          current: 2,
+          pageSize: 2,
+        },
+        {},
+        {}
+      )
+
+      expect(handlePaginationChange).toBeCalledWith(2, 2)
+      done()
+    })
+  })
+
+  // https://github.com/ant-design/ant-design/issues/4532
+  // https://codepen.io/afc163/pen/dVeNoP?editors=001
+  it('should have pager when change pagination from false to undefined', (done) => {
+    const wrapper = mount(Table, getTableOptions({ pagination: false }))
+    Vue.nextTick(() => {
+      expect(wrapper.findAll('.ant-pagination')).toHaveLength(0)
+      wrapper.setProps({ pagination: undefined })
+      Vue.nextTick(() => {
+        expect(wrapper.findAll('.ant-pagination')).toHaveLength(1)
+        expect(wrapper.findAll('.ant-pagination-item-active')).toHaveLength(1)
+        done()
+      })
+    })
+  })
+
+  // https://github.com/ant-design/ant-design/issues/4532
+  // https://codepen.io/afc163/pen/pWVRJV?editors=001
+  it('should display pagination as prop pagination change between true and false', (done) => {
+    const wrapper = mount(Table, getTableOptions())
+    Vue.nextTick(() => {
+      expect(wrapper.findAll('.ant-pagination')).toHaveLength(1)
+      expect(wrapper.findAll('.ant-pagination-item')).toHaveLength(2)
+      wrapper.setProps({ pagination: false })
+      Vue.nextTick(() => {
+        expect(wrapper.findAll('.ant-pagination')).toHaveLength(0)
+        wrapper.setProps({ pagination })
+        // wrapper.update()
+        Vue.nextTick(() => {
+          expect(wrapper.findAll('.ant-pagination')).toHaveLength(1)
+          expect(wrapper.findAll('.ant-pagination-item')).toHaveLength(2)
+          wrapper.find('.ant-pagination-item-2').trigger('click')
+          Vue.nextTick(() => {
+            expect(renderedNames(wrapper)).toEqual(['Tom', 'Jerry'])
+            wrapper.setProps({ pagination: false })
+            Vue.nextTick(() => {
+              expect(wrapper.findAll('.ant-pagination')).toHaveLength(0)
+              wrapper.setProps({ pagination: true })
+              Vue.nextTick(() => {
+                expect(wrapper.findAll('.ant-pagination')).toHaveLength(1)
+                expect(wrapper.findAll('.ant-pagination-item')).toHaveLength(1) // pageSize will be 10
+                expect(renderedNames(wrapper)).toHaveLength(4)
+                done()
+              })
+            })
+          })
+        })
+      })
+    })
+  })
+
+  // https://github.com/ant-design/ant-design/issues/5259
+  it('change to correct page when data source changes', (done) => {
+    const wrapper = mount(Table, getTableOptions({ pagination: { pageSize: 1 }}))
+    Vue.nextTick(() => {
+      wrapper.find('.ant-pagination-item-3').trigger('click')
+      wrapper.setProps({ dataSource: [data[0]] })
+      Vue.nextTick(() => {
+        expect(wrapper.find('.ant-pagination-item-1').classes()).toContain('ant-pagination-item-active')
+        done()
+      })
+    })
+  })
+
+  it('specify the position of pagination', (done) => {
+    const wrapper = mount(Table, getTableOptions({ pagination: { position: 'top' }}))
+    Vue.nextTick(() => {
+      expect(wrapper.findAll('.ant-spin-container > *')).toHaveLength(2)
+      expect(wrapper.findAll('.ant-spin-container > *').at(0).findAll('.ant-pagination')).toHaveLength(1)
+      wrapper.setProps({ pagination: { position: 'bottom' }})
+      Vue.nextTick(() => {
+        expect(wrapper.findAll('.ant-spin-container > *')).toHaveLength(2)
+        expect(wrapper.findAll('.ant-spin-container > *').at(1).findAll('.ant-pagination')).toHaveLength(1)
+        wrapper.setProps({ pagination: { position: 'both' }})
+        Vue.nextTick(() => {
+          expect(wrapper.findAll('.ant-spin-container > *')).toHaveLength(3)
+          expect(wrapper.findAll('.ant-spin-container > *').at(0).findAll('.ant-pagination')).toHaveLength(1)
+          expect(wrapper.findAll('.ant-spin-container > *').at(2).findAll('.ant-pagination')).toHaveLength(1)
+          done()
+        })
+      })
+    })
+  })
+})
---
+++ components/table/__tests__/Table.test.js
@@ -0,0 +1,102 @@
+import { shallowMount as shallow, mount } from '@vue/test-utils'
+import Table from '..'
+import Vue from 'vue'
+
+const { Column, ColumnGroup } = Table
+
+describe('Table', () => {
+  it('renders JSX correctly', (done) => {
+    const data = [{
+      key: '1',
+      firstName: 'John',
+      lastName: 'Brown',
+      age: 32,
+    }, {
+      key: '2',
+      firstName: 'Jim',
+      lastName: 'Green',
+      age: 42,
+    }]
+
+    const wrapper = mount({
+      render () {
+        return (
+          <Table dataSource={data} pagination={false}>
+            <ColumnGroup title='Name'>
+              <Column
+                title='First Name'
+                dataIndex='firstName'
+                key='firstName'
+              />
+              <Column
+                title='Last Name'
+                dataIndex='lastName'
+                key='lastName'
+              />
+            </ColumnGroup>
+            <Column
+              title='Age'
+              dataIndex='age'
+              key='age'
+            />
+          </Table>
+        )
+      },
+    }, { sync: false }
+    )
+
+    Vue.nextTick(() => {
+      expect(wrapper.html()).toMatchSnapshot()
+      done()
+    })
+  })
+
+  it('updates columns when receiving props', (done) => {
+    const columns = [{
+      title: 'Name',
+      key: 'name',
+      dataIndex: 'name',
+    }]
+    const wrapper = shallow(Table, {
+      propsData: {
+        columns,
+      },
+      sync: false,
+    })
+    const newColumns = [{
+      title: 'Title',
+      key: 'title',
+      dataIndex: 'title',
+    }]
+    wrapper.setProps({ columns: newColumns })
+    Vue.nextTick(() => {
+      expect(wrapper.vm.columns).toBe(newColumns)
+      done()
+    })
+  })
+
+  it('loading with Spin', (done) => {
+    const loading = {
+      spinning: false,
+      delay: 500,
+    }
+    const wrapper = mount(Table, {
+      propsData: {
+        loading,
+      },
+      sync: false,
+    })
+    Vue.nextTick(async () => {
+      expect(wrapper.findAll('.ant-spin')).toHaveLength(0)
+      expect(wrapper.find('.ant-table-placeholder').text()).not.toEqual('')
+
+      loading.spinning = true
+      wrapper.setProps({ loading })
+      expect(wrapper.findAll('.ant-spin')).toHaveLength(0)
+
+      await new Promise(resolve => setTimeout(resolve, 1000))
+      expect(wrapper.findAll('.ant-spin')).toHaveLength(1)
+      done()
+    })
+  })
+})

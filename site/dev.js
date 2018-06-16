482ce65589d383d145e62b9b50e32c41a2c5e66b
tjz <415800467@qq.com>
2018-06-16 21:47:32 +0800

    perf: update dev env

---
+++ site/dev.js
@@ -0,0 +1,31 @@
+import 'babel-polyfill'
+import '../components/style.js'
+import './index.less'
+import 'highlight.js/styles/solarized-light.css'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueClipboard from 'vue-clipboard2'
+import Md from './components/md'
+import Api from './components/api'
+import './components'
+import demoBox from './components/demoBox'
+import demoContainer from './components/demoContainer'
+import Test from '../components/list/demo/index'
+
+Vue.use(VueClipboard)
+Vue.use(VueRouter)
+Vue.component(Md.name, Md)
+Vue.component(Api.name, Api)
+Vue.component('demo-box', demoBox)
+Vue.component('demo-container', demoContainer)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/*', component: Test },
+  ],
+})
+new Vue({
+  el: '#app',
+  router,
+})

9f307fd25da4d6c001c2b4a1c5f3394659bf31f7
tjz <415800467@qq.com>
2018-06-17 17:25:54 +0800

    test: add radio & tabs tests

---
+++ components/radio/__tests__/radio.test.js
@@ -0,0 +1,41 @@
+import { mount } from '@vue/test-utils'
+import { asyncExpect } from '@/tests/utils'
+import Radio from '../radio'
+import focusTest from '../../../tests/shared/focusTest'
+
+describe('Radio', () => {
+  focusTest(Radio)
+
+  it('should render correctly', () => {
+    const wrapper = mount({
+      render () {
+        return <Radio class='customized'>Test</Radio>
+      },
+    })
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('responses hover events', async () => {
+    const onMouseEnter = jest.fn()
+    const onMouseLeave = jest.fn()
+
+    const wrapper = mount({
+      render () {
+        return <Radio
+          onMouseenter={onMouseEnter}
+          onMouseleave={onMouseLeave}
+        />
+      },
+    }, { sync: false })
+    await asyncExpect(() => {
+      wrapper.trigger('mouseenter')
+    })
+    await asyncExpect(() => {
+      expect(onMouseEnter).toHaveBeenCalled()
+    })
+    wrapper.trigger('mouseleave')
+    await asyncExpect(() => {
+      expect(onMouseLeave).toHaveBeenCalled()
+    })
+  })
+})

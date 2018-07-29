a3f61ef00a3eaca8b6548dd782dbfc500a36a893
tangjinzhou <415800467@qq.com>
2018-07-29 19:33:02 +0800

    fix: update export props #87

--- components/dropdown/index.js
+++ components/dropdown/index.js
@@ -1,7 +1,7 @@
 import Dropdown from './dropdown'
 import DropdownButton from './dropdown-button'
 
-export { DropDownProps } from './dropdown'
+export { DropdownProps } from './dropdown'
 export { DropdownButtonProps } from './dropdown-button'
 
 Dropdown.Button = DropdownButton

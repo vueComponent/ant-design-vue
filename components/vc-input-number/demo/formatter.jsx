5a66dd8b95178280a6832c1e4c879bd2a2d436b7
wangxueliang <wangxueliang@yidian-inc.com>
2018-08-03 18:12:10 +0800

    feat: update vc-input-number to 4.0.12

---
+++ components/vc-input-number/demo/formatter.jsx
@@ -0,0 +1,62 @@
+import InputNumber from '../src/index'
+import '../assets/index.less'
+
+function getSum (str) {
+  let total = 0
+  str.split('').forEach((c) => {
+    const num = Number(c)
+
+    if (!isNaN(num)) {
+      total += num
+    }
+  })
+
+  return total
+}
+
+export default {
+  data () {
+    return {
+      value: 1000,
+    }
+  },
+  render () {
+    return (
+      <div style='margin: 10px;'>
+        <InputNumber
+          defaultValue={1000}
+          formatter={value => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+        />
+        <InputNumber
+          defaultValue={100}
+          formatter={value => `${value}%`}
+          parser={value => value.replace('%', '')}
+        />
+        <InputNumber
+          style='width: 100px'
+          formatter={value =>
+            `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+          }
+        />
+
+        <div>
+          <h1>In Control</h1>
+          <InputNumber
+            value={this.value}
+            onChange={(value) => { this.value = value }}
+            formatter={value => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+          />
+        </div>
+
+        <div>
+          <h1>Strange Format</h1>
+          <InputNumber
+            defaultValue={1000}
+            formatter={value => `$ ${value} - ${getSum(value)}`}
+            parser={value => (value.match(/^\$ ([\d\.]*) .*$/) || [])[1]}
+          />
+        </div>
+      </div>
+    )
+  },
+}
---
+++ components/vc-upload/demo/index.vue
@@ -0,0 +1,23 @@
+<script>
+import Simple from '../vc-upload/demo/simple'
+import Drag from '../vc-upload/demo/drag'
+import AsyncAction from '../vc-upload/demo/asyncAction'
+import BeforeUpload from '../vc-upload/demo/beforeUpload'
+import CustomRequest from '../vc-upload/demo/customRequest'
+import DirectoryUpload from '../vc-upload/demo/directoryUpload'
+
+export default {
+  render () {
+    return (
+      <div>
+        <Simple />
+        <Drag />
+        <AsyncAction />
+        <BeforeUpload />
+        <CustomRequest />
+        <DirectoryUpload />
+      </div>
+    )
+  },
+}
+</script>

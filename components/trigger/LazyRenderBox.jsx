3aa580e7015b4ca3ac2c7fc2462d0f53582f9e39
tangjinzhou <415800467@qq.com>
2018-08-14 22:53:01 +0800

    fix: trigger mock shouldComponentUpdate

--- components/trigger/LazyRenderBox.jsx
+++ components/trigger/LazyRenderBox.jsx
@@ -1,5 +1,6 @@
 
 import PropTypes from '../_util/vue-types'
+import { cloneVNode } from '../_util/vnode'
 
 export default {
   props: {
@@ -8,20 +9,23 @@ export default {
   },
   render () {
     const { hiddenClassName, visible } = this.$props
-
+    let children = null
     if (hiddenClassName || !this.$slots.default || this.$slots.default.length > 1) {
       const cls = ''
       if (!visible && hiddenClassName) {
         // cls += ` ${hiddenClassName}`
       }
-      return (
+      children = (
         <div class={cls}>
-          {this.$slots.default}
+          {visible ? this.$slots.default : (this.preChildren || this.$slots.default)}
         </div>
       )
+    } else {
+      children = visible ? this.$slots.default[0] : (this.preChildren || this.$slots.default[0])
     }
-
-    return this.$slots.default[0]
+    // mock shouldComponentUpdate
+    this.preChildren = visible ? cloneVNode(children) : this.preChildren
+    return children
   },
 }
 

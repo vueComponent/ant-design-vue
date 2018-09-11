85a9aca4921145a353dee5d9d044bb3326381dc2
wangxueliang <wangxueliang@yidian-inc.com>
2018-09-11 09:14:57 +0800

    fix: the spin component loads for the first time #174

---
+++ components/list/__tests__/index.test.js
@@ -0,0 +1,19 @@
+import { mount } from '@vue/test-utils'
+import { asyncExpect } from '@/tests/utils'
+import List from '..'
+
+describe('List', () => {
+  it('locale not passed to internal div', async () => {
+    const locale = { emptyText: 'Custom text' }
+    const wrapper = mount({
+      render () {
+        return <List dataSource={[]} renderItem={(item) => <List.Item>{item}</List.Item>} locale={locale} />
+      },
+    }, {
+      sync: false,
+    })
+    await asyncExpect(() => {
+      expect(wrapper.props().locale).toBe(undefined)
+    })
+  })
+})

2fb666759358ccf4d8d3a035ea0ce6b4c4a31225
tangjinzhou <415800467@qq.com>
2018-09-24 15:37:36 +0800

    docs: add SeedAndDew

--- site/components/sponsors.vue
+++ site/components/sponsors.vue
@@ -1,13 +1,15 @@
 <template>
-<div class="sponsorsWrap">
-  <span class="sponsorsTitle">{{title}}</span>
-  <ul >
-    <li class="sponsorsItem">
-      <a href="https://tipe.io/?ref=ant-design-vue" target="_blank">
-        <img width="34" src="https://cdn.tipe.io/tipe/tipe-cat-no-text.svg" alt="tipe">
-      </a>
-    </li>
-  </ul>
+<div class='snd-ad'>
+  <div class="sponsorsWrap">
+    <span class="sponsorsTitle">{{title}}</span>
+    <ul >
+      <li class="sponsorsItem">
+        <a href="https://tipe.io/?ref=ant-design-vue" target="_blank">
+          <img width="34" src="https://cdn.tipe.io/tipe/tipe-cat-no-text.svg" alt="tipe">
+        </a>
+      </li>
+    </ul>
+  </div>
 </div>
 </template>
 <script>

3d41b55ba35a22513831e555e9249c4eadb58d41
wangxueliang <wangxueliang@yidian-inc.com>
2018-11-30 10:50:26 +0800

    feat: update steps

--- components/steps/index.jsx
+++ components/steps/index.jsx
@@ -2,12 +2,15 @@
 import PropTypes from '../_util/vue-types'
 import { initDefaultProps, getOptionProps, getComponentFromProp } from '../_util/props-util'
 import VcSteps from '../vc-steps'
+import Icon from '../icon'
 
 const getStepsProps = (defaultProps = {}) => {
   const props = {
     prefixCls: PropTypes.string,
     iconPrefix: PropTypes.string,
     current: PropTypes.number,
+    initial: PropTypes.number,
+    labelPlacement: PropTypes.oneOf(['horizontal', 'vertical']),
     status: PropTypes.oneOf(['wait', 'process', 'finish', 'error']),
     size: PropTypes.oneOf(['default', 'small']),
     direction: PropTypes.oneOf(['horizontal', 'vertical']),
@@ -29,13 +32,15 @@ const Steps = {
   Step: { ...VcSteps.Step, name: 'AStep' },
   render () {
     const props = getOptionProps(this)
+    const { prefixCls } = props
+    const icons = {
+      finish: <Icon type='check' className={`${prefixCls}-finish-icon`} />,
+      error: <Icon type='close' className={`${prefixCls}-error-icon`} />,
+    }
     const stepsProps = {
       props: {
         ...props,
-        icons: props.icons || {
-          error: getComponentFromProp(this, 'icons.error'),
-          finish: getComponentFromProp(this, 'icons.finish'),
-        },
+        icons,
       },
       on: this.$listeners,
       scopedSlots: this.$scopedSlots,

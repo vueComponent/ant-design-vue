87132100007f24e80dd479d06979d5b101388060
wangxueliang <wangxueliang@yidian-inc.com>
2019-03-04 14:57:51 +0800

    feat: update vc-tabs to 9.6.1

--- components/vc-tabs/src/ScrollableInkTabBar.jsx
+++ components/vc-tabs/src/ScrollableInkTabBar.jsx
@@ -22,12 +22,13 @@ export default {
   render() {
     const props = { ...this.$props };
     const listeners = this.$listeners;
+    const { renderTabBarNode } = this.scopedSlots;
     return (
       <SaveRef
         children={(saveRef, getRef) => (
           <TabBarRootNode saveRef={saveRef} {...{ props, on: listeners }}>
             <ScrollableTabBarNode saveRef={saveRef} getRef={getRef} {...{ props, on: listeners }}>
-              <TabBarTabsNode saveRef={saveRef} {...{ props, on: listeners }} />
+              <TabBarTabsNode saveRef={saveRef} {...{ props: {...props, renderTabBarNode}, on: listeners }} />
               <InkTabBarNode saveRef={saveRef} getRef={getRef} {...{ props, on: listeners }} />
             </ScrollableTabBarNode>
           </TabBarRootNode>

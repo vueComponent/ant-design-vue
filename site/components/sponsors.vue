ccd5d6498206546154ee3678eecb50af7f50f33f
tangjinzhou <415800467@qq.com>
2019-03-11 10:06:33 +0800

    update site add sponsor

--- site/components/sponsors.vue
+++ site/components/sponsors.vue
@@ -11,9 +11,24 @@
             target="_blank"
           >
             <img
-              width="34"
+              height="51"
               src="https://cdn.tipe.io/tipe/tipe-cat-no-text.svg"
               alt="tipe"
+            >
+          </a>
+        </li>
+        <li
+          v-if="isEffective(effectiveTime.bmatch)"
+          class="sponsorsItem"
+        >
+          <a
+            href="https://www.bmatch.tech/?ref=ant-design-vue"
+            target="_blank"
+          >
+            <img
+              height="61"
+              src="https://cdn.nlark.com/yuque/0/2019/png/87084/1552279877057-assets/web-upload/48a6ba91-6750-478c-956f-57aedb632fe5.png"
+              alt="bmatch"
             >
           </a>
         </li>
@@ -22,7 +37,24 @@
   </div>
 </template>
 <script>
+import moment from 'moment';
 export default {
   props: ['title'],
+  data() {
+    return {
+      top: 50,
+      effectiveTime : {
+        bmatch: {
+          start: '2019-03-11',
+          end: '2019-06-11',
+        },
+      },
+    };
+  },
+  methods: {
+    isEffective({start, end}){
+      return moment().isBetween(start, end);
+    },
+  },
 };
 </script>

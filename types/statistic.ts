04ac500c2ef748809041bbad388a7c64c93f5930
mingyan.yu <mingyan.yu@wormpex.com>
2019-05-25 18:02:44 +0800

    feat: statistic.ts

--- types/statistic.ts
+++ types/statistic.ts
@@ -3,49 +3,57 @@
 // Definitions: https://github.com/vueComponent/ant-design-vue/types
 
 import { AntdComponent } from './component';
+import { VNode } from 'vue';
 
 export declare class Statistic extends AntdComponent {
   /**
-   * the Icon type for an icon statistic, see Icon Component
+   * decimal separator
+   * @default '.'
    * @type string
    */
-  icon: string;
+  decimalSeparator: string;
 
   /**
    * the shape of statistic
-   * @default 'circle'
    * @type string
    */
-  shape: 'circle' | 'square';
+  formatter: () => VNode;
 
   /**
-   * the size of the statistic
-   * @default 'default'
-   * @type number | string
-   */
-  size: 'small' | 'large' | 'default' | number;
-
-  /**
-   * the address of the image for an image statistic
+   * group separator
+   * @default ','
    * @type string
    */
-  src: string;
+  groupSeparator: string;
 
   /**
-   * a list of sources to use for different screen resolutions
-   * @type string
+   * precision of input value
+   * @type number
    */
-  srcSet: string;
+  precision: number;
 
   /**
-   * This attribute defines the alternative text describing the image
-   * @type string
+   * prefix node of value
+   * @type string | VNode
    */
-  alt: string;
+  prefix: string | VNode;
 
   /**
-   * handler when img load error，return false to prevent default fallback behavior
-   * @type
+   * suffix node of value
+   * @type string | VNode
    */
-  loadError: () => boolean;
+  suffix: string | VNode;;
+
+  /**
+   * Display title
+   * @type string | VNode
+   */
+  title: string | VNode;
+
+  /**
+   * Display value
+   * @type string or number
+   */
+  value: string | number;
+
 }

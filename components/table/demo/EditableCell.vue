2f2eca8fac83af7fa94db2537e806b65aad11003
Maicon Cerutti <maicon.cerutti@univille.br>
2019-06-20 15:09:32 -0300

    Sujestão de melhoria
    Uma sujestão de melhoria
    
    Deixar passar uma propriedade como type para que os desenvolvedores possam atribuir como desejarem, e caso sejá número eu já deixei o CSS para remover as setas.
    
    : )

--- components/table/demo/EditableCell.vue
+++ components/table/demo/EditableCell.vue
@@ -1,29 +1,18 @@
 <template>
   <div class="editable-cell">
-    <div
-      v-if="editable"
-      class="editable-cell-input-wrapper"
-    >
+    <div v-if="editable" class="editable-cell-input-wrapper">
       <a-input
+        class="input-no-arrows"
+        :type="type"
         :value="value"
         @change="handleChange"
         @pressEnter="check"
-      /><a-icon
-        type="check"
-        class="editable-cell-icon-check"
-        @click="check"
       />
+      <a-icon type="check" class="editable-cell-icon-check" @click="check"/>
     </div>
-    <div
-      v-else
-      class="editable-cell-text-wrapper"
-    >
+    <div v-else class="editable-cell-text-wrapper">
       {{ value || ' ' }}
-      <a-icon
-        type="edit"
-        class="editable-cell-icon"
-        @click="edit"
-      />
+      <a-icon type="edit" class="editable-cell-icon" @click="edit"/>
     </div>
   </div>
 </template>
@@ -31,25 +20,36 @@
 export default {
   props: {
     text: String,
+    type: {
+      type: String,
+      required: true
+    }
   },
-  data () {
+  data() {
     return {
       value: this.text,
-      editable: false,
-    };
+      editable: false
+    }
   },
   methods: {
-    handleChange (e) {
-      const value = e.target.value;
-      this.value = value;
+    handleChange(e) {
+      const value = e.target.value
+      this.value = value
     },
-    check () {
-      this.editable = false;
-      this.$emit('change', this.value);
+    check() {
+      this.editable = false
+      this.$emit('change', this.value)
     },
-    edit () {
-      this.editable = true;
-    },
-  },
-};
+    edit() {
+      this.editable = true
+    }
+  }
+}
 </script>
+<style scoped>
+.input-no-arrows::-webkit-inner-spin-button,
+.input-no-arrows::-webkit-outer-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+</style>

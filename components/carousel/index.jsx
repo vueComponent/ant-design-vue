c89d19e6e145a9bbdec3ab2133562f36ebc2bd20
tanjinzhou <415800467@qq.com>
2019-09-29 17:22:11 +0800

    feat: Carousel support responsive #1230

--- components/carousel/index.jsx
+++ components/carousel/index.jsx
@@ -64,6 +64,7 @@ export const CarouselProps = {
   variableWidth: PropTypes.bool,
   useCSS: PropTypes.bool,
   slickGoTo: PropTypes.number,
+  responsive: PropTypes.array.def(null),
 };
 
 const Carousel = {

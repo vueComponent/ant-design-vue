c789380d68565182c8ad4b1fac8a8deed9bf9563
zkwolf <chenhao5866@gmail.com>
2019-10-07 22:51:21 +0800

    fix: lint change code

--- components/_util/responsiveObserve.js
+++ components/_util/responsiveObserve.js
@@ -13,7 +13,7 @@ if (typeof window !== 'undefined') {
     };
   };
   // ref: https://github.com/ant-design/ant-design/issues/18774
-  if (!window.matchMedia) window.matchMedia = matchMediaPolyfill
+  if (!window.matchMedia) window.matchMedia = matchMediaPolyfill;
   // eslint-disable-next-line global-require
   enquire = require('enquire.js');
 }

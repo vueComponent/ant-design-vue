4fa8055933ac65d95ea7ad801dd9ee3b2acb1313
Amour1688 <31695475+Amour1688@users.noreply.github.com>
2019-11-19 16:05:46 +0800

    remove "。" in TreeSelect doc (#1433)

--- components/tree-select/demo/index.vue
+++ components/tree-select/demo/index.vue
@@ -9,7 +9,7 @@ import CN from '../index.zh-CN.md';
 import US from '../index.en-US.md';
 
 const md = {
-  cn: `# 树型选择控件。
+  cn: `# 树型选择控件
   ## 何时使用
 
 类似 Select 的选择控件，可选择的数据结构是一个树形结构时，可以使用 TreeSelect，例如公司层级、学科系统、分类目录等等。

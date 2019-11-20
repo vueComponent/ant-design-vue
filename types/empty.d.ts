59dc98dbd12462b39239e86f41b35e1ed0a4be63
Sven Reber <svreber@gmail.com>
2019-11-20 07:37:41 +0100

    fix: add typings for Empty component (#1439)

---
+++ types/empty.d.ts
@@ -0,0 +1,22 @@
+// Project: https://github.com/vueComponent/ant-design-vue
+// Definitions by: Svreber <https://github.com/Svreber>
+// Definitions: https://github.com/vueComponent/ant-design-vue/types
+
+import { AntdComponent } from './component';
+import { VNode } from 'vue';
+
+export declare class Empty extends AntdComponent {
+  /**
+   * customize description
+   * @type string | VNode
+   */
+  description: string | VNode;
+
+  /**
+   * customize image. Will tread as image url when string provided
+   * @default false
+   * @type string | VNode
+   */
+  image: string | VNode;
+
+}

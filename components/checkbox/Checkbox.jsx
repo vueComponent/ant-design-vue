4c17169436c2e633445fdc206e36614ce56a5306
tanjinzhou <415800467@qq.com>
2020-03-18 11:08:39 +0800

    feat: update checkbox

--- components/checkbox/Checkbox.jsx
+++ components/checkbox/Checkbox.jsx
@@ -95,6 +95,7 @@ export default {
       checkboxProps.props.name = checkboxGroup.name;
       checkboxProps.props.checked = checkboxGroup.sValue.indexOf(props.value) !== -1;
       checkboxProps.props.disabled = props.disabled || checkboxGroup.disabled;
+      checkboxProps.props.indeterminate = indeterminate;
     } else {
       checkboxProps.on.change = this.handleChange;
     }

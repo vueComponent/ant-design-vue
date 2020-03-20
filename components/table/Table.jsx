749d74e21f64bd73764cc158f9d35aea372a436f
tanjinzhou <415800467@qq.com>
2020-03-20 18:38:47 +0800

    fix: table error when use template #1914

--- components/table/Table.jsx
+++ components/table/Table.jsx
@@ -490,13 +490,15 @@ export default {
         return getPopupContainer;
       }
       // Use undefined to let rc component use default logic.
-      return scroll && table ? () => table.tableNode : undefined;
+      return scroll && table ? () => table.getTableNode() : undefined;
     },
     scrollToFirstRow() {
       const { scroll } = this.$props;
       if (scroll && scroll.scrollToFirstRowOnChange !== false) {
         scrollTo(0, {
-          getContainer: () => this.$refs.vcTable.bodyTable,
+          getContainer: () => {
+            return this.$refs.vcTable.getBodyTable();
+          },
         });
       }
     },

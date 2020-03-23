60bb3ae50c06255a3d150afa7a13d84121c0d500
Amour1688 <31695475+Amour1688@users.noreply.github.com>
2020-03-23 11:00:24 +0800

    chore: simple demo (#1918)

---
+++ webpack.config.js
@@ -0,0 +1,100 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const webpack = require('webpack');
+const WebpackBar = require('webpackbar');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './examples/index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              'env',
+              {
+                targets: {
+                  browsers: [
+                    'last 2 versions',
+                    'Firefox ESR',
+                    '> 1%',
+                    'ie >= 9',
+                    'iOS >= 8',
+                    'Android >= 4',
+                  ],
+                },
+              },
+            ],
+          ],
+          plugins: [
+            // libraryDirectory is "", because webpack alias doesn't seem to allow '/'
+            ['import', { libraryName: 'ant-design-vue', style: true, libraryDirectory: '' }],
+            'transform-vue-jsx',
+            'transform-object-assign',
+            'transform-object-rest-spread',
+            'transform-class-properties',
+          ],
+        },
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]',
+        },
+      },
+      {
+        test: /\.less$/,
+        use: [
+          { loader: 'vue-style-loader' },
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true },
+          },
+          { loader: 'less-loader', options: { sourceMap: true, javascriptEnabled: true } },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      'ant-design-vue': path.join(__dirname, './components'),
+    },
+    extensions: ['.js', '.jsx', '.vue'],
+  },
+  devServer: {
+    host: 'localhost',
+    port: 3000,
+    historyApiFallback: {
+      rewrites: [{ from: /./, to: '/index.html' }],
+    },
+    disableHostCheck: true,
+    hot: true,
+    open: true,
+  },
+  devtool: '#source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'examples/index.html',
+      filename: 'index.html',
+      inject: true,
+    }),
+    new VueLoaderPlugin(),
+    new WebpackBar(),
+  ],
+};

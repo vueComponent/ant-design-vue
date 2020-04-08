4446dec9a609cebd15f616eff3fca6395a573fde
tangjinzhou <415800467@qq.com>
2020-04-08 22:58:20 +0800

    fix: tabClick not work #2030

--- components/tabs/tabs.jsx
+++ components/tabs/tabs.jsx
@@ -163,7 +163,10 @@ export default {
         ...getOptionProps(this),
         prefixCls,
         tabBarPosition: tabPosition,
-        renderTabBar: () => <TabBar {...tabBarProps} />,
+        // https://github.com/vueComponent/ant-design-vue/issues/2030
+        // 如仅传递 tabBarProps 会导致，第二次执行 renderTabBar 时，丢失 on 属性，
+        // 添加key之后，会在babel jsx 插件中做一次merge，最终TabBar接收的是一个新的对象，而不是 tabBarProps
+        renderTabBar: () => <TabBar key="tabBar" {...tabBarProps} />,
         renderTabContent: () => (
           <TabContent class={contentCls} animated={tabPaneAnimated} animatedWithMargin />
         ),

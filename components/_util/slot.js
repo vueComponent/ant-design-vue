9db33d98072fb9d99da4247220bdfda195ab3369
baojie <baojie223@qq.com>
2020-04-27 12:59:14 +0800

    fix: 修复bug

--- components/_util/slot.js
+++ components/_util/slot.js
@@ -5,12 +5,10 @@ export function getSlot(vm, name = 'default', props = {}) {
   if (!(vm instanceof Vue)) throw Error('Please input vue instance');
   const nodes1 = (vm.$scopedSlots[name] && vm.$scopedSlots[name](props)) || [];
   const nodes2 = vm.$slots[name] || [];
-  if (nodes1.length && nodes2.length) {
-    return [...nodes1, ...nodes2];
+  if (nodes2.length) {
+    return [...nodes2];
   } else if (nodes1.length) {
     return [...nodes1];
-  } else if (nodes2.length) {
-    return [...nodes2];
   } else {
     return [];
   }

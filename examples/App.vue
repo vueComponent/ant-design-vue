69dfd71e003c6f0aee316dea54050673b5110a1f
Almaz Mubinov <almaz@mubinov.com>
2020-05-11 10:41:50 +0300

    Adding example to doc

--- examples/App.vue
+++ examples/App.vue
@@ -90,6 +90,38 @@
 
       <br/>
       <br/>
+      <h2>Select</h2>
+      <div style="padding: 10px;">
+        <a-select default-value="lucy" style="width: 120px">
+          <a-select-option value="jack">
+            Jack
+          </a-select-option>
+          <a-select-option value="lucy">
+            Lucy
+          </a-select-option>
+          <a-select-option value="disabled" disabled>
+            Disabled
+          </a-select-option>
+          <a-select-option value="Yiminghe">
+            yiminghe
+          </a-select-option>
+        </a-select>
+
+        <a-select default-value="lucy" style="width: 120px" disabled>
+          <a-select-option value="lucy">
+            Lucy
+          </a-select-option>
+        </a-select>
+
+        <a-select default-value="lucy" style="width: 120px" loading>
+          <a-select-option value="lucy">
+            Lucy
+          </a-select-option>
+        </a-select>
+      </div>
+
+      <br/>
+      <br/>
       <h2>Buttons</h2>
       <div style="padding: 10px;">
         <a-button type="primary">Primary</a-button>

831c81551890e73ded5ab3082671a3d74fca9316
Herbert Lin <herbert.lin.7@gmail.com>
2020-05-12 23:07:15 +0800

    Add function support for format

--- components/date-picker/utils.js
+++ components/date-picker/utils.js
@@ -1,9 +1,16 @@
+const isFunction = function(obj) {
+  return !!(obj && obj.constructor && obj.call && obj.apply);
+};
+
 export function formatDate(value, format) {
   if (!value) {
     return '';
   }
   if (Array.isArray(format)) {
     format = format[0];
+  }
+  if (isFunction(format)) {
+    return format(value);
   }
   return value.format(format);
 }

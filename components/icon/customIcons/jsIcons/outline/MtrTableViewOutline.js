c9d796f4c9d65347ae7fb939eb29621bd33415f7
undefined <undefined>
2020-05-18 18:49:49 +0500

    build: 2 new icons

---
+++ components/icon/customIcons/jsIcons/outline/MtrTableViewOutline.js
@@ -0,0 +1,49 @@
+const MtrTableViewOutline = {
+  name: 'mtr-table-view',
+  theme: 'outline',
+  icon: {
+    tag: 'svg',
+    attrs: {
+      viewBox: '0 0 24 24',
+      fill: 'none',
+      xmlns: 'http://www.w3.org/2000/svg',
+    },
+    children: [
+      {},
+      {
+        tag: 'path',
+        attrs: {
+          d: 'M5 6H19',
+          stroke: 'currentColor',
+          'stroke-width': '2',
+          'stroke-miterlimit': '10',
+          'stroke-linecap': 'round',
+        },
+      },
+      {},
+      {
+        tag: 'path',
+        attrs: {
+          d: 'M5 12H19',
+          stroke: 'currentColor',
+          'stroke-width': '2',
+          'stroke-miterlimit': '10',
+          'stroke-linecap': 'round',
+        },
+      },
+      {},
+      {
+        tag: 'path',
+        attrs: {
+          d: 'M5 18H19',
+          stroke: 'currentColor',
+          'stroke-width': '2',
+          'stroke-miterlimit': '10',
+          'stroke-linecap': 'round',
+        },
+      },
+      {},
+    ],
+  },
+};
+export default MtrTableViewOutline;

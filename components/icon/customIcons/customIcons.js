98e922d565166c6ed1831c549acf40de66670d38
Almaz Mubinov <almaz@mubinov.com>
2020-05-18 11:12:52 +0300

    Move custom icons file

---
+++ components/icon/customIcons/customIcons.js
@@ -0,0 +1,14 @@
+export {default as MtrArrowDownOutline} from './jsIcons/outline/MtrArrowDownOutline.js';
+export {default as MtrArrowLeftOutline} from './jsIcons/outline/MtrArrowLeftOutline.js';
+export {default as MtrArrowRightOutline} from './jsIcons/outline/MtrArrowRightOutline.js';
+export {default as MtrArrowUpOutline} from './jsIcons/outline/MtrArrowUpOutline.js';
+export {default as MtrCheckOutline} from './jsIcons/outline/MtrCheckOutline.js';
+export {default as MtrCloseOutline} from './jsIcons/outline/MtrCloseOutline.js';
+export {default as MtrDotsOutline} from './jsIcons/outline/MtrDotsOutline.js';
+export {default as MtrEyeOffOutline} from './jsIcons/outline/MtrEyeOffOutline.js';
+export {default as MtrEyeOutline} from './jsIcons/outline/MtrEyeOutline.js';
+export {default as MtrPlusOutline} from './jsIcons/outline/MtrPlusOutline.js';
+export {default as MtrSearchOutline} from './jsIcons/outline/MtrSearchOutline.js';
+export {default as MtrSettingsOutline} from './jsIcons/outline/MtrSettingsOutline.js';
+export {default as MtrTrash1Outline} from './jsIcons/outline/MtrTrash1Outline.js';
+export {default as MtrTrash2Outline} from './jsIcons/outline/MtrTrash2Outline.js';

318e588294415b2a6788daf9508f1ab2d54f0399
Almaz Mubinov <almaz@mubinov.com>
2020-05-18 11:13:07 +0300

    Update custom icon generating

--- icons/utils/customIconFile.js
+++ icons/utils/customIconFile.js
@@ -2,24 +2,27 @@ const glob = require("glob");
 const path = require("path");
 const fs = require("fs");
 
+const customIconsDir = "components/icon/customIcons";
+
 class CustomIconFile {
   static generateCustomIconFile() {
     this.iconFiles = [];
-    this.addIconFilesFromDirectory("lib/outline");
-    this.addIconFilesFromDirectory("lib/fill");
-    this.addIconFilesFromDirectory("lib/twotone");
+    this.addIconFilesFromDirectory("outline");
+    this.addIconFilesFromDirectory("fill");
+    this.addIconFilesFromDirectory("twotone");
+
     const output = [];
     this.iconFiles.forEach((iconFile) => {
-      const modulePath = iconFile.replace(/^icons/, '');
+      const modulePath = iconFile.replace(/^components\/icon\/customIcons/, '');
       const moduleName = path.parse(modulePath).name;
       const moduleExport = `export {default as ${moduleName}} from '.${modulePath}';`;
       output.push(moduleExport);
     });
-    fs.writeFileSync('icons/customIcons.js', output.join("\n"));
+    fs.writeFileSync(`${customIconsDir}/customIcons.js`, output.join("\n"));
   }
 
   static addIconFilesFromDirectory(directory) {
-    const files = glob(`icons/${directory}/*.js`, {sync: true});
+    const files = glob(`${customIconsDir}/jsIcons/${directory}/*.js`, {sync: true});
     this.iconFiles = this.iconFiles.concat(files);
   }
 }

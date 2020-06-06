09352623340d60c32c34fa02eb8642565c03e248
zkwolf <chenhao5866@gmail.com>
2020-06-06 14:51:12 +0800

    feat: update divider (#2357)
    * feat: update divider
    
    * chore: remove base import
    
    * style: move up slot.default() judge

--- examples/index.js
+++ examples/index.js
@@ -5,6 +5,7 @@ import Button from 'ant-design-vue/button';
 import Drawer from 'ant-design-vue/drawer';
 import Affix from 'ant-design-vue/affix';
 import Alert from 'ant-design-vue/alert';
+import Divider from 'ant-design-vue/divider';
 import ConfigProvider from 'ant-design-vue/config-provider';
 import Spin from 'ant-design-vue/Spin';
 import Empty from 'ant-design-vue/empty';
@@ -16,6 +17,7 @@ createApp(App)
   .use(Drawer)
   .use(Affix)
   .use(Alert)
+  .use(Divider)
   .use(Spin)
   .use(Empty)
   .mount('#app');

e5748a1add095d94e11d998bf300c9c8e27303cb
xrkffgg <xrkffgg@vip.qq.com>
2020-06-06 16:02:58 +0800

    feat: update avatar

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,7 @@
 import '@babel/polyfill';
 import { createApp } from 'vue';
 import App from './App.vue';
+import Avatar from 'ant-design-vue/avatar';
 import Button from 'ant-design-vue/button';
 import Drawer from 'ant-design-vue/drawer';
 import Affix from 'ant-design-vue/affix';
@@ -11,6 +12,7 @@ import Empty from 'ant-design-vue/empty';
 import 'ant-design-vue/style.js';
 
 createApp(App)
+  .use(Avatar)
   .use(Button)
   .use(ConfigProvider)
   .use(Drawer)

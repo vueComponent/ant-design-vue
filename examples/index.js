4ef17f81448a4b9ae0facc874bd0ee39c9b925a3
xrkffgg <xrkffgg@gmail.com>
2020-06-08 15:38:07 +0800

    feat: update avatar (#2372)

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,7 @@
 import '@babel/polyfill';
 import { createApp } from 'vue';
 import App from './App.vue';
+import Avatar from 'ant-design-vue/avatar';
 import Button from 'ant-design-vue/button';
 import Comment from 'ant-design-vue/comment';
 import Drawer from 'ant-design-vue/drawer';
@@ -15,6 +16,7 @@ import Timeline from 'ant-design-vue/timeline';
 import 'ant-design-vue/style.js';
 
 createApp(App)
+  .use(Avatar)
   .use(Button)
   .use(Comment)
   .use(ConfigProvider)

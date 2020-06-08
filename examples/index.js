f1384c5f0a27d0bab317b8612dc85b3445813377
xrkffgg <xrkffgg@vip.qq.com>
2020-06-08 15:33:29 +0800

    fix: index

--- examples/index.js
+++ examples/index.js
@@ -11,7 +11,6 @@ import ConfigProvider from 'ant-design-vue/config-provider';
 import Result from 'ant-design-vue/result';
 import Spin from 'ant-design-vue/spin';
 import PageHeader from 'ant-design-vue/page-header';
-import Spin from 'ant-design-vue/Spin';
 import Empty from 'ant-design-vue/empty';
 import Timeline from 'ant-design-vue/timeline';
 import 'ant-design-vue/style.js';

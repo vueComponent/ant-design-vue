e5924d5ded65d150734d9fff65741464e582e885
言肆 <yladmxa@gmail.com>
2020-06-11 22:35:09 +0800

    feat: update descriptions (#2385)
    * feat: update descriptions
    
    * fix: merge Col props

--- components/descriptions/Col.jsx
+++ components/descriptions/Col.jsx
@@ -1,5 +1,5 @@
 import PropTypes from '../_util/vue-types';
-import { getOptionProps, getSlots, getComponentFromProp } from '../_util/props-util';
+import { getOptionProps } from '../_util/props-util';
 
 const ColProps = {
   child: PropTypes.any,
@@ -9,70 +9,74 @@ const ColProps = {
   layout: PropTypes.oneOf(['horizontal', 'vertical']),
 };
 
-const Col = {
-  functional: true,
-  props: ColProps,
-  render(h, ctx) {
-    const { child, bordered, colon, type, layout } = ctx.props;
-    const { prefixCls, span = 1 } = getOptionProps(child);
-    const { key } = ctx.data;
-    const label = getComponentFromProp(child, 'label');
-    const slots = getSlots(child);
-    const labelProps = {
-      attrs: {},
-      class: [
-        `${prefixCls}-item-label`,
-        {
-          [`${prefixCls}-item-colon`]: colon,
-          [`${prefixCls}-item-no-label`]: !label,
-        },
-      ],
-      key: `${key}-label`,
-    };
-    if (layout === 'vertical') {
-      labelProps.attrs.colSpan = span * 2 - 1;
-    }
+const Col = (_, { attrs }) => {
+  // props: {
+  //   child: PropTypes.any,
+  //   bordered: PropTypes.bool,
+  //   colon: PropTypes.bool,
+  //   type: PropTypes.oneOf(['label', 'content']),
+  //   layout: PropTypes.oneOf(['horizontal', 'vertical']),
+  // }
+  const { child, bordered, colon, type, layout } = attrs;
+  const { prefixCls, span = 1 } = getOptionProps(child);
+  const { key, children = {} } = child || {};
+  const label = children.label && children.label();
+  const defaultSlot = children.default && children.default();
 
-    if (bordered) {
-      if (type === 'label') {
-        return <th {...labelProps}>{label}</th>;
-      }
-      return (
-        <td class={`${prefixCls}-item-content`} key={`${key}-content`} colSpan={span * 2 - 1}>
-          {slots.default}
-        </td>
-      );
+  const someLabelProps = {
+    class: [
+      `${prefixCls}-item-label`,
+      {
+        [`${prefixCls}-item-colon`]: colon,
+        [`${prefixCls}-item-no-label`]: !label,
+      },
+    ],
+    key: `${key}-label`,
+  };
+
+  if (layout === 'vertical') {
+    someLabelProps.colSpan = span * 2 - 1;
+  }
+
+  if (bordered) {
+    if (type === 'label') {
+      return <th {...someLabelProps}>{label}</th>;
     }
-    if (layout === 'vertical') {
-      if (type === 'content') {
-        return (
-          <td colSpan={span} class={`${prefixCls}-item`}>
-            <span class={`${prefixCls}-item-content`} key={`${key}-content`}>
-              {slots.default}
-            </span>
-          </td>
-        );
-      }
+    return (
+      <td class={`${prefixCls}-item-content`} key={`${key}-content`} colSpan={span * 2 - 1}>
+        {defaultSlot}
+      </td>
+    );
+  }
+  if (layout === 'vertical') {
+    if (type === 'content') {
       return (
         <td colSpan={span} class={`${prefixCls}-item`}>
-          <span
-            class={[`${prefixCls}-item-label`, { [`${prefixCls}-item-colon`]: colon }]}
-            key={`${key}-label`}
-          >
-            {label}
+          <span class={`${prefixCls}-item-content`} key={`${key}-content`}>
+            {defaultSlot}
           </span>
         </td>
       );
     }
     return (
       <td colSpan={span} class={`${prefixCls}-item`}>
-        <span {...labelProps}>{label}</span>
-        <span class={`${prefixCls}-item-content`} key={`${key}-content`}>
-          {slots.default}
+        <span
+          class={[`${prefixCls}-item-label`, { [`${prefixCls}-item-colon`]: colon }]}
+          key={`${key}-label`}
+        >
+          {label}
         </span>
       </td>
     );
-  },
+  }
+  return (
+    <td colSpan={span} class={`${prefixCls}-item`}>
+      <span {...someLabelProps}>{label}</span>
+      <span class={`${prefixCls}-item-content`} key={`${key}-content`}>
+        {defaultSlot}
+      </span>
+    </td>
+  );
 };
 
 export default Col;

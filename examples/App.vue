23d54d82ba52c1a6da603afbcc6284ad4e5a70c2
baojie <baojie223@qq.com>
2020-07-17 16:01:24 +0800

    fix: 格式错误

--- examples/App.vue
+++ examples/App.vue
@@ -1,5 +1,61 @@
 <template>
-  <input v-model="input" :maxlength="number" />
+  <a-config-provider :locale="locale">
+    <div>
+      <a-radio-group :value="locale" @change="changeLocale">
+        <a-radio-button key="en" :value="enUS">English</a-radio-button>
+        <a-radio-button key="cn" :value="zhCN">中文</a-radio-button>
+      </a-radio-group>
+      <div>
+        <h4>
+          v-model:
+          <input v-model="color" type="text" />
+        </h4>
+        <a-color-picker v-model="color" style="width:130px" @show="changeHandler" />
+      </div>
+      <div>
+        <h4>v-model:{{ color }}</h4>
+        <a-color-picker v-model="color" style="width:130px" @show="changeHandler" />
+      </div>
+      <div>
+        <h4>备选颜色</h4>
+        <a-color-picker :config="config1" />
+      </div>
+      <div>
+        <h4>多种输入格式</h4>
+        <a-color-picker :config="config2" :get-popup-container="node => node.parentNode" />
+      </div>
+      <div>
+        <button @click="config2.components.preview = !config2.components.preview">
+          修改config
+        </button>
+      </div>
+      <div>
+        <h4>多种尺寸</h4>
+        <a-color-picker size="large" />
+      </div>
+      <div>
+        <a-color-picker size="small" />
+      </div>
+      <div>
+        <h4>切换禁用</h4>
+        <a-color-picker :disabled="disabled" />
+      </div>
+      <div>
+        <button @click="disabled = !disabled">切换</button>
+      </div>
+      <div>
+        <h4>切换格式 [color:{{ color2 }}]</h4>
+        <a-color-picker v-model="color2" :locale="zhCN" :format="format" />
+      </div>
+      <div>
+        <button @click="format = 'HEX'">HEX</button>
+        <button @click="format = 'RGBA'">RGBA</button>
+        <button @click="format = 'HSVA'">HSVA</button>
+        <button @click="format = 'HSLA'">HSLA</button>
+        <button @click="format = 'CMYK'">CMYK</button>
+      </div>
+    </div>
+  </a-config-provider>
 </template>
 
 <script>

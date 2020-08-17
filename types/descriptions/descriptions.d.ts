25a97ec5ceeea0329cfd8b63114a43403bb00d15
xrkffgg <xrkffgg@vip.qq.com>
2020-08-17 13:52:44 +0800

    chore: update type descriptions (#2686)
    * chore: update type descriptions
    
    * fix: object
    
    * fix: add pub

--- types/descriptions/descriptions.d.ts
+++ types/descriptions/descriptions.d.ts
@@ -2,56 +2,61 @@
 // Definitions by: akki-jat <https://github.com/akki-jat>
 // Definitions: https://github.com/vueComponent/ant-design-vue/types
 
+import { VNodeChild } from 'vue';
 import { AntdComponent } from '../component';
 import { DescriptionsItem } from './descriptions-item';
+
+import { Breakpoint } from '../pub';
 
 export declare class Descriptions extends AntdComponent {
   static Item: typeof DescriptionsItem;
 
-  /**
-   * descriptions size type
-   * @default 'default'
-   * @type string
-   */
-  size: 'default' | 'middle' | 'small';
+  $props: {
+    /**
+     * descriptions size type
+     * @default 'default'
+     * @type string
+     */
+    size?: 'default' | 'middle' | 'small';
 
-  /**
-   * custom prefixCls
-   * @type string
-   */
-  prefixCls: string;
+    /**
+     * custom prefixCls
+     * @type string
+     */
+    prefixCls?: string;
 
-  /**
-   * whether descriptions have border
-   * @default false
-   * @type boolean
-   */
-  bordered: boolean;
+    /**
+     * whether descriptions have border
+     * @default false
+     * @type boolean
+     */
+    bordered?: boolean;
 
-  /**
-   * custom title
-   * @type any
-   */
-  title: any;
+    /**
+     * custom title
+     * @type any
+     */
+    title?: VNodeChild | JSX.Element;
 
-  /**
-   * the number of descriptionsitem in one line
-   * @default 3
-   * @type number | object
-   */
-  column: number | object;
+    /**
+     * the number of descriptionsitem in one line
+     * @default 3
+     * @type number | object
+     */
+    column?: number | Partial<Breakpoint>;
 
-  /**
-   * descriptions layout
-   * @default 'horizontal'
-   * @type string
-   */
-  layout: 'horizontal' | 'vertical';
+    /**
+     * descriptions layout
+     * @default 'horizontal'
+     * @type string
+     */
+    layout?: 'horizontal' | 'vertical';
 
-  /**
-   * whether have colon in descriptionsitem
-   * @default true
-   * @type boolean
-   */
-  colon: boolean;
+    /**
+     * whether have colon in descriptionsitem
+     * @default true
+     * @type boolean
+     */
+    colon?: boolean;
+  };
 }

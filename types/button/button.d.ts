1d8341bbee35bbffe47f0cded3a57724f28849f3
Amour1688 <lcz_1996@foxmail.com>
2020-08-17 18:41:55 +0800

    chore(types): button

--- types/button/button.d.ts
+++ types/button/button.d.ts
@@ -3,6 +3,7 @@
 // Definitions: https://github.com/vueComponent/ant-design-vue/types
 
 import { ButtonGroup } from './button-group';
+import { VNodeChild } from 'vue';
 
 export declare class Button {
   static Group: typeof ButtonGroup;
@@ -13,61 +14,61 @@ export declare class Button {
      * @default 'default'
      * @type string
      */
-    type: 'primary' | 'danger' | 'dashed' | 'ghost' | 'default';
+    type?: 'primary' | 'danger' | 'dashed' | 'ghost' | 'default';
 
     /**
      * set the original html type of button
      * @default 'button'
      * @type string
      */
-    htmlType: 'button' | 'submit' | 'reset' | 'menu';
+    htmlType?: 'button' | 'submit' | 'reset' | 'menu';
 
     /**
      * set the icon of button
      * @type string
      */
-    icon: string;
+    icon?: VNodeChild | JSX.Element;
 
     /**
      * can be set to circle or circle-outline or omitted
      * @type string
      */
-    shape: 'circle' | 'circle-outline';
+    shape?: 'circle' | 'circle-outline';
 
     /**
      * can be set to small large or omitted
      * @default 'default'
      * @type string
      */
-    size: 'small' | 'large' | 'default';
+    size?: 'small' | 'large' | 'default';
 
     /**
      * set the loading status of button
      * @default false
      * @type boolean | { delay: number }
      */
-    loading: boolean | { delay: number };
+    loading?: boolean | { delay: number };
 
     /**
      * disabled state of button
      * @default false
      * @type boolean
      */
-    disabled: boolean;
+    disabled?: boolean;
 
     /**
      * make background transparent and invert text and border colors, added in 2.7
      * @default false
      * @type boolean
      */
-    ghost: boolean;
+    ghost?: boolean;
 
     /**
      * option to fit button width to its parent width
      * @default false
      * @type boolean
      */
-    block: boolean;
+    block?: boolean;
 
     onClick?: (e?: Event) => void;
   };

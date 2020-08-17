724236faba9245e775fd5366a7bbcc890928dc23
xrkffgg <xrkffgg@vip.qq.com>
2020-08-17 13:43:46 +0800

    chore: update type alert

--- types/alert.d.ts
+++ types/alert.d.ts
@@ -3,62 +3,65 @@
 // Definitions: https://github.com/vueComponent/ant-design-vue/types
 
 import { AntdComponent } from './component';
+import { VNodeChild } from 'vue';
 
 export declare class Alert extends AntdComponent {
-  /**
-   * Called when close animation is finished
-   * @type Function
-   */
-  afterClose: () => void;
+  $props: {
+    /**
+     * Called when close animation is finished
+     * @type Function
+     */
+    afterClose?: () => void;
 
-  /**
-   * Whether to show as banner
-   * @default false
-   * @type boolean
-   */
-  banner: boolean;
+    /**
+     * Whether to show as banner
+     * @default false
+     * @type boolean
+     */
+    banner?: boolean;
 
-  /**
-   * Whether Alert can be closed
-   * @type boolean
-   */
-  closable: boolean;
+    /**
+     * Whether Alert can be closed
+     * @type boolean
+     */
+    closable?: boolean;
 
-  /**
-   * Close text to show
-   * @type any (string | slot)
-   */
-  closeText: any;
+    /**
+     * Close text to show
+     * @type any (string | slot)
+     */
+    closeText?: VNodeChild | JSX.Element;
 
-  /**
-   * additional content of Alert
-   * @type any (string | slot)
-   */
-  description: any;
+    /**
+     * additional content of Alert
+     * @type any (string | slot)
+     */
+    description?: VNodeChild | JSX.Element;
 
-  /**
-   * Custom icon, effective when showIcon is true
-   * @type any (VNode | slot)
-   */
-  icon: any;
+    /**
+     * Custom icon, effective when showIcon is true
+     * @type any (VNode | slot)
+     */
+    icon?: VNodeChild | JSX.Element;
 
-  /**
-   * Content of Alert
-   * @type any (string | slot)
-   */
-  message: any;
+    /**
+     * Content of Alert
+     * @type any (string | slot)
+     */
+    message?: VNodeChild | JSX.Element;
 
-  /**
-   * Whether to show icon
-   * @default false, in banner mode default is true
-   * @type boolean
-   */
-  showIcon: boolean;
+    /**
+     * Whether to show icon
+     * @default false, in banner mode default is true
+     * @type boolean
+     */
+    showIcon?: boolean;
 
-  /**
-   * Type of Alert styles, options: success, info, warning, error
-   * @default info, in banner mode default is warning
-   * @type string
-   */
-  type: 'success' | 'info' | 'warning' | 'error';
+    /**
+     * Type of Alert styles, options: success, info, warning, error
+     * @default info, in banner mode default is warning
+     * @type string
+     */
+    type?: 'success' | 'info' | 'warning' | 'error';
+  };
 }

cf48d5bef087160eac536452ddfa4b7b43b56ff6
cydiacen <binhao.cen@xiaobao100.com>
2020-08-17 14:04:08 +0800

    chore: update type popconfirm

--- types/popconfirm.d.ts
+++ types/popconfirm.d.ts
@@ -3,41 +3,41 @@
 // Definitions: https://github.com/vueComponent/ant-design-vue/types
 
 import { TooltipCommon } from './tootip/common';
-
+import { VNodeChild } from 'vue';
 export declare class Popconfirm extends TooltipCommon {
   /**
    * text of the Cancel button
    * @default 'Cancel'
    * @type any (string | slot)
    */
-  cancelText: any;
+  cancelText?: string | VNodeChild | JSX.Element;
 
   /**
    * text of the Confirm button
    * @default 'Confirm'
    * @type any (string | slot)
    */
-  okText: any;
+  okText?: string | VNodeChild | JSX.Element;
 
   /**
    * Button type of the Confirm button
    * @default 'primary'
    * @type string
    */
-  okType: 'primary' | 'danger' | 'dashed' | 'ghost' | 'default';
+  okType?: 'primary' | 'danger' | 'dashed' | 'ghost' | 'default';
 
   /**
    * title of the confirmation box
    * @type any (string | slot)
    */
-  title: any;
+  title?: string | VNodeChild | JSX.Element;
 
   /**
    * customize icon of confirmation
    * @default <ExclamationCircleOutlined />
    * @type any (VNode | slot)
    */
-  icon: any;
+  icon?: VNodeChild | JSX.Element;
 
-  disabled: boolean;
+  disabled?: boolean;
 }

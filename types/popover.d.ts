30a000a44a10ee81bef1ea7323664d713ca0d8fc
cydiacen <binhao.cen@xiaobao100.com>
2020-08-17 14:04:35 +0800

    chore: update type popover

--- types/popover.d.ts
+++ types/popover.d.ts
@@ -3,17 +3,17 @@
 // Definitions: https://github.com/vueComponent/ant-design-vue/types
 
 import { TooltipCommon } from './tootip/common';
-
+import { VNodeChild } from 'vue';
 export declare class Popover extends TooltipCommon {
   /**
    * Content of the card
    * @type any (string | slot | VNode)
    */
-  content: any;
+  content?: string | VNodeChild | JSX.Element;
 
   /**
    * Title of the card
    * @type any (string | slot | VNode)
    */
-  title: any;
+  title?: string | VNodeChild | JSX.Element;
 }

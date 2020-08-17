05877986747f6dd50abef4f8a6f6b6a5407024e7
xrkffgg <xrkffgg@vip.qq.com>
2020-08-17 13:53:47 +0800

    chore: update type avatar (#2689)

--- types/avatar.d.ts
+++ types/avatar.d.ts
@@ -2,50 +2,53 @@
 // Definitions by: akki-jat <https://github.com/akki-jat>
 // Definitions: https://github.com/vueComponent/ant-design-vue/types
 
+import { VNodeChild } from 'vue';
 import { AntdComponent } from './component';
 
 export declare class Avatar extends AntdComponent {
-  /**
-   * the Icon type for an icon avatar, see Icon Component
-   * @type any (VNode | slot)
-   */
-  icon: any;
+  $props: {
+    /**
+     * the Icon type for an icon avatar, see Icon Component
+     * @type any (VNode | slot)
+     */
+    icon?: VNodeChild | JSX.Element;
 
-  /**
-   * the shape of avatar
-   * @default 'circle'
-   * @type string
-   */
-  shape: 'circle' | 'square';
+    /**
+     * the shape of avatar
+     * @default 'circle'
+     * @type string
+     */
+    shape?: 'circle' | 'square';
 
-  /**
-   * the size of the avatar
-   * @default 'default'
-   * @type number | string
-   */
-  size: 'small' | 'large' | 'default' | number;
+    /**
+     * the size of the avatar
+     * @default 'default'
+     * @type number | string
+     */
+    size?: 'small' | 'large' | 'default' | number;
 
-  /**
-   * the address of the image for an image avatar
-   * @type string
-   */
-  src: string;
+    /**
+     * the address of the image for an image avatar
+     * @type string
+     */
+    src?: string;
 
-  /**
-   * a list of sources to use for different screen resolutions
-   * @type string
-   */
-  srcSet: string;
+    /**
+     * a list of sources to use for different screen resolutions
+     * @type string
+     */
+    srcSet?: string;
 
-  /**
-   * This attribute defines the alternative text describing the image
-   * @type string
-   */
-  alt: string;
+    /**
+     * This attribute defines the alternative text describing the image
+     * @type string
+     */
+    alt?: string;
 
-  /**
-   * handler when img load error，return false to prevent default fallback behavior
-   * @type
-   */
-  loadError: () => boolean;
+    /**
+     * handler when img load error，return false to prevent default fallback behavior
+     * @type
+     */
+    loadError?: () => boolean;
+  };
 }

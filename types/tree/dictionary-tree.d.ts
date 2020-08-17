cc0223ee0a868d41361b47fec287c8c989ee72ba
Sendya <18x@loacg.com>
2020-08-17 15:40:26 +0800

    fix: update transfer, tree types

--- types/tree/dictionary-tree.d.ts
+++ types/tree/dictionary-tree.d.ts
@@ -6,10 +6,12 @@ import { AntdComponent } from '../component';
 import { Tree } from './tree';
 
 export declare class DictionaryTree extends Tree {
-  /**
-   * Directory open logic, optional false 'click' 'dblclick'
-   * @default 'click'
-   * @type string
-   */
-  expandAction: string;
+  $props: {
+    /**
+     * Directory open logic, optional `false` 'click' 'dblclick'
+     * @default 'click'
+     * @type string
+     */
+    expandAction?: string | boolean;
+  };
 }

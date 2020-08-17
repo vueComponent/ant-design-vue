54d241824975f32044d8ed3d03c307f1862d7493
言肆 <18x@loacg.com>
2020-08-17 21:12:17 +0800

    chore: update table, list types (#2694)
    * fix: table typo
    
    * fix: list typo
    
    * fix: update table, list return JSX.Element typo
    
    * fix: xxxRender merge params
    
    * fix: change table event return type `void`

--- types/table/column-group.d.ts
+++ types/table/column-group.d.ts
@@ -3,18 +3,21 @@
 // Definitions: https://github.com/vueComponent/ant-design-vue/types
 
 import { AntdComponent } from '../component';
+import { VNodeChild } from 'vue';
 
 export declare class ColumnGroup extends AntdComponent {
-  /**
-   * Title of the column group
-   * @type any
-   */
-  title: any;
+  $props: {
+    /**
+     * Title of the column group
+     * @type any
+     */
+    title?: VNodeChild | JSX.Element;
 
-  /**
-   * When using columns, you can use this property to configure the properties that support the slot,
-   * such as slots: { title: 'XXX'}
-   * @type object
-   */
-  slots: object;
+    /**
+     * When using columns, you can use this property to configure the properties that support the slot,
+     * such as slots: { title: 'XXX'}
+     * @type object
+     */
+    slots?: object;
+  };
 }

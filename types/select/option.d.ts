63984b42b6a61d9bf19f6a581407a54f98ff67c4
cydiacen <binhao.cen@xiaobao100.com>
2020-08-17 14:58:47 +0800

    chore: update type Select

--- types/select/option.d.ts
+++ types/select/option.d.ts
@@ -5,34 +5,37 @@
 import { AntdComponent } from '../component';
 
 export declare class Option extends AntdComponent {
-  /**
-   * Disable this option
-   * @default false
-   * @type boolean
-   */
-  disabled: boolean;
+  $props: {
+    /**
+  * Disable this option
+  * @default false
+  * @type boolean
+  */
+    disabled?: boolean;
 
-  /**
-   * Same usage as value. If Vue request you to set this property, you can set it to value of option, and then omit value property.
-   * @type string
-   */
-  key: string;
+    /**
+     * Same usage as value. If Vue request you to set this property, you can set it to value of option, and then omit value property.
+     * @type string
+     */
+    key?: string;
 
-  /**
-   * title of Select after select this Option
-   * @type string
-   */
-  title: string;
+    /**
+     * title of Select after select this Option
+     * @type string
+     */
+    title?: string;
 
-  /**
-   * default to filter with this property
-   * @type string | number
-   */
-  value: string | number;
+    /**
+     * default to filter with this property
+     * @type string | number
+     */
+    value?: string | number;
 
-  /**
-   * additional class to option
-   * @type string
-   */
-  class: string;
+    /**
+     * additional class to option
+     * @type string
+     */
+    class?: string;
+  }
+
 }

a0451ff0e767d08cce25358100f03ea0209e4335
kagawagao <kingsongao1221@gmail.com>
2020-08-18 10:40:36 +0800

    chore(types): update Layout types

--- types/layout/layout.d.ts
+++ types/layout/layout.d.ts
@@ -7,6 +7,28 @@ import { LayoutSider } from './layout-sider';
 import LayoutHeader from './layout-header';
 import LayoutContent from './layout-content';
 import LayoutFooter from './layout-footer';
+import { CSSProperties } from 'vue';
+
+export interface LayoutProps {
+  /**
+   * container className
+   * @default undefined
+   * @type string
+   */
+  class?: string;
+
+  /**
+   * to customize the styles
+   * @type CSSProperties
+   */
+  style?: CSSProperties;
+
+  /**
+   * whether contain Sider in children, don't have to assign it normally. Useful in ssr avoid style flickering
+   * @type boolean
+   */
+  hasSider?: boolean;
+}
 
 export declare class Layout extends AntdComponent {
   static Header: typeof LayoutHeader;
@@ -14,22 +36,5 @@ export declare class Layout extends AntdComponent {
   static Footer: typeof LayoutFooter;
   static Sider: typeof LayoutSider;
 
-  /**
-   * container className
-   * @default undefined
-   * @type string
-   */
-  class: string;
-
-  /**
-   * to customize the styles
-   * @type string | object
-   */
-  style: string | object;
-
-  /**
-   * whether contain Sider in children, don't have to assign it normally. Useful in ssr avoid style flickering
-   * @type boolean
-   */
-  hasSider: boolean;
+  $props: LayoutProps;
 }

e3c38b68f87f25eee14471de5e277d843a870b1d
binhaoCen <binhao.cen@xiaobao100.com>
2020-08-19 21:02:18 +0800

    chore: update type dropdown (#2721)

--- types/dropdown/dropdown-button.d.ts
+++ types/dropdown/dropdown-button.d.ts
@@ -4,53 +4,58 @@
 
 import { AntdComponent } from '../component';
 import { Menu } from '../menu/menu';
+import { VNodeChild } from 'vue';
 
 export declare class DropdownButton extends AntdComponent {
-  /**
-   * whether the dropdown menu is disabled
-   * @type boolean
-   */
-  disabled: boolean;
+  $props: {
+    /**
+     * whether the dropdown menu is disabled
+     * @type boolean
+     */
+    disabled?: boolean;
 
-  /**
-   * the dropdown menu
-   * @type () => Menu
-   */
-  overlay: any;
+    /**
+     * the dropdown menu
+     * @type () => Menu
+     */
+    overlay?: () => Menu
 
-  /**
-   * placement of pop menu: bottomLeft bottomCenter bottomRight topLeft topCenter topRight
-   * @default 'bottomLeft'
-   * @type string
-   */
-  placement: 'topLeft' | 'topCenter' | 'topRight' | 'bottomLeft' | 'bottomCenter' | 'bottomRight';
+    /**
+     * placement of pop menu: bottomLeft bottomCenter bottomRight topLeft topCenter topRight
+     * @default 'bottomLeft'
+     * @type string
+     */
+    placement?: 'topLeft' | 'topCenter' | 'topRight' | 'bottomLeft' | 'bottomCenter' | 'bottomRight';
 
-  /**
-   * size of the button, the same as Button
-   * @default 'default'
-   * @type string
-   */
-  size: 'small' | 'large' | 'default';
+    /**
+     * size of the button, the same as Button
+     * @default 'default'
+     * @type string
+     */
+    size?: 'small' | 'large' | 'default';
 
-  /**
-   * the trigger mode which executes the drop-down action
-   * @default ['hover']
-   * @type string[]
-   */
-  trigger: Array<'click' | 'hover' | 'contextmenu'>;
+    /**
+     * the trigger mode which executes the drop-down action
+     * @default ['hover']
+     * @type string[]
+     */
+    trigger?: Array<'click' | 'hover' | 'contextmenu'>;
 
-  /**
-   * type of the button, the same as Button
-   * @default 'default'
-   * @type string
-   */
-  type: 'primary' | 'danger' | 'dashed' | 'ghost' | 'default';
+    /**
+     * type of the button, the same as Button
+     * @default 'default'
+     * @type string
+     */
+    type?: 'primary' | 'danger' | 'dashed' | 'ghost' | 'default';
 
-  /**
-   * whether the dropdown menu is visible
-   * @type boolean
-   */
-  visible: boolean;
-  icon: any;
-  title: string;
+    /**
+     * whether the dropdown menu is visible
+     * @type boolean
+     */
+    visible?: boolean;
+    /**
+     * Icon (appears on the right) (1.5.0)
+     */
+    icon?: VNodeChild | JSX.Element;
+  }
 }

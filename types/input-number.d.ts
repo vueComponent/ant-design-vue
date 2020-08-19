d4052f1fab4d94bf52992b30fd1a536d5d431396
cydiacen <binhao.cen@xiaobao100.com>
2020-08-19 18:04:46 +0800

    chore: update type input-number

--- types/input-number.d.ts
+++ types/input-number.d.ts
@@ -5,81 +5,83 @@
 import { AntdComponent } from './component';
 
 export declare class InputNumber extends AntdComponent {
-  /**
-   * get focus when component mounted
-   * @default false
-   * @type boolean
-   */
-  autofocus: boolean;
+  $props: {
+    /**
+     * get focus when component mounted
+     * @default false
+     * @type boolean
+     */
+    autofocus?: boolean;
 
-  /**
-   * initial value
-   * @type number | string
-   */
-  defaultValue: number | string;
+    /**
+     * initial value
+     * @type number | string
+     */
+    defaultValue?: number | string;
 
-  /**
-   * disable the input
-   * @default false
-   * @type boolean
-   */
-  disabled: boolean;
+    /**
+     * disable the input
+     * @default false
+     * @type boolean
+     */
+    disabled?: boolean;
 
-  /**
-   * Specifies the format of the value presented
-   * @type Function
-   */
-  formatter: (value: number | string) => string;
+    /**
+     * Specifies the format of the value presented
+     * @type Function
+     */
+    formatter?: (value: number | string) => string;
 
-  /**
-   * max value
-   * @default Infinity
-   * @type number
-   */
-  max: number;
+    /**
+     * max value
+     * @default Infinity
+     * @type number
+     */
+    max?: number;
 
-  /**
-   * mix value
-   * @default Infinity
-   * @type number
-   */
-  min: number;
+    /**
+     * mix value
+     * @default -Infinity
+     * @type number
+     */
+    min?: number;
 
-  /**
-   * Specifies the value extracted from formatter
-   * @type Function
-   */
-  parser: (string: string) => number;
+    /**
+     * Specifies the value extracted from formatter
+     * @type Function
+     */
+    parser?: (string: string) => number;
 
-  /**
-   * precision of input value
-   * @type number
-   */
-  precision: number;
+    /**
+     * precision of input value
+     * @type number
+     */
+    precision?: number;
 
-  /**
-   * decimal separator
-   * @type string
-   */
-  decimalSeparator: string;
+    /**
+     * decimal separator
+     * @type string
+     */
+    decimalSeparator?: string;
 
-  /**
-   * width of input box
-   * @default 'default'
-   * @type string
-   */
-  size: 'large' | 'small' | 'default';
+    /**
+     * width of input box
+     * @default 'default'
+     * @type string
+     */
+    size?: 'large' | 'small' | 'default';
 
-  /**
-   * The number to which the current value is increased or decreased. It can be an integer or decimal.
-   * @default 1
-   * @type number | string
-   */
-  step: number | string;
+    /**
+     * The number to which the current value is increased or decreased. It can be an integer or decimal.
+     * @default 1
+     * @type number | string
+     */
+    step?: number | string;
 
-  /**
-   * current value
-   * @type number | string
-   */
-  value: number | string;
+    /**
+     * current value
+     * @type number | string
+     */
+    value?: number | string;
+  }
 }

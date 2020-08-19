ec024c83713eef66b7096eefc461a85400905acc
Amour1688 <31695475+Amour1688@users.noreply.github.com>
2020-08-19 15:24:55 +0800

    chore: table props (#2713)
    * chore: table props
    
    * fix: table props

--- types/spin.d.ts
+++ types/spin.d.ts
@@ -5,50 +5,51 @@
 import { AntdComponent } from './component';
 import { VNodeChild } from 'vue';
 
+export interface SpinProps {
+  /**
+   * specifies a delay in milliseconds for loading state (prevent flush)
+   * @type number (milliseconds)
+   */
+  delay?: number;
+
+  /**
+   * vue node of the spinning indicator
+   * @type any (VNode | slot)
+   */
+  indicator?: VNodeChild | JSX.Element;
+
+  /**
+   * size of Spin, options: small, default and large
+   * @default 'default'
+   * @type string
+   */
+  size?: 'small' | 'default' | 'large';
+
+  /**
+   * whether Spin is spinning
+   * @default true
+   * @type boolean
+   */
+  spinning?: boolean;
+
+  /**
+   * customize description content when Spin has children
+   * @type string
+   */
+  tip?: string;
+
+  /**
+   * className of wrapper when Spin has children
+   * @type string
+   */
+  wrapperClassName?: string;
+}
+
 export declare class Spin extends AntdComponent {
   /**
    * As indicator, you can define the global default spin element
    * @param param0 indicator
    */
   static setDefaultIndicator({ indicator }: { indicator: any }): void;
-  $props: {
-
-    /**
-     * specifies a delay in milliseconds for loading state (prevent flush)
-     * @type number (milliseconds)
-     */
-    delay?: number;
-
-    /**
-     * vue node of the spinning indicator
-     * @type any (VNode | slot)
-     */
-    indicator?: VNodeChild | JSX.Element;
-
-    /**
-     * size of Spin, options: small, default and large
-     * @default 'default'
-     * @type string
-     */
-    size?: 'small' | 'default' | 'large';
-
-    /**
-     * whether Spin is spinning
-     * @default true
-     * @type boolean
-     */
-    spinning?: boolean;
-
-    /**
-     * customize description content when Spin has children
-     * @type string
-     */
-    tip?: string;
-
-    /**
-     * className of wrapper when Spin has children
-     * @type string
-     */
-    wrapperClassName?: string;
-  }
+  $props: SpinProps;
 }

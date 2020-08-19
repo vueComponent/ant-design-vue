5d14259e7a4688c03c6dc25dfc7a73944da0c16a
binhaoCen <binhao.cen@xiaobao100.com>
2020-08-19 15:25:36 +0800

    chore: update type mentions (#2716)

--- types/mentions/option.d.ts
+++ types/mentions/option.d.ts
@@ -4,10 +4,12 @@
 import { AntdComponent } from '../component';
 
 export declare class Option extends AntdComponent {
-  /**
+  $props: {
+    /**
    * value of suggestion, the value will insert into input filed while selected
    * @default ''
    * @type string
    */
-  value: string;
+    value?: string;
+  }
 }

6681b508922060fb2b56b3899bd4b0d3ec72aa3c
binhaoCen <binhao.cen@xiaobao100.com>
2020-08-19 21:01:12 +0800

    chore: update type input (#2720)

--- types/input/input-group.d.ts
+++ types/input/input-group.d.ts
@@ -5,17 +5,19 @@
 import { AntdComponent } from '../component';
 
 export declare class InputGroup extends AntdComponent {
-  /**
-   * Whether use compact style
-   * @default false
-   * @type boolean
-   */
-  compact: boolean;
+  $props: {
+    /**
+     * Whether use compact style
+     * @default false
+     * @type boolean
+     */
+    compact?: boolean;
 
-  /**
-   * The size of Input.Group specifies the size of the included Input fields. Available: large default small
-   * @default 'default'
-   * @type string
-   */
-  size: 'small' | 'large' | 'default';
+    /**
+     * The size of Input.Group specifies the size of the included Input fields. Available: large default small
+     * @default 'default'
+     * @type string
+     */
+    size?: 'small' | 'large' | 'default';
+  }
 }

4afb93e7fb0ab9ed279fb17e70d9556563a64fda
tanjinzhou <415800467@qq.com>
2020-09-10 16:39:02 +0800

    chore: update vue-loader version

--- examples/App.vue
+++ examples/App.vue
@@ -1,21 +1,10 @@
-<cn>
-#### 信息预览抽屉
-需要快速预览对象概要时使用，点击遮罩区关闭。
-</cn>
-
-<us>
-#### Preview drawer
-Use Drawer to quickly preview details of an object, such as those in a list.
-</us>
-
-```vue
 <template>
   <div>
     <demo />
   </div>
 </template>
 <script>
-import demo from '../antdv-demo/docs/form/demo/nested-useForm';
+import demo from '../antdv-demo/docs/form/demo';
 export default {
   components: {
     demo,
@@ -45,4 +34,3 @@ export default {
   },
 };
 </script>
-```

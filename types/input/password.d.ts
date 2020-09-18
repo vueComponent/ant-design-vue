925970a68d9329a5471319f4863e4f848baad49c
John <John60676@qq.com>
2020-09-18 12:27:26 +0800

    chore(types): update input props types

--- types/input/password.d.ts
+++ types/input/password.d.ts
@@ -3,13 +3,15 @@
 // Definitions: https://github.com/vueComponent/ant-design-vue/types
 
 import { AntdComponent, AntdProps } from '../component';
+import { InputProps } from './input';
 
 export declare class Password extends AntdComponent {
-  $props: AntdProps & {
-    /**
-     * Whether show toggle button
-     * @default true
-     */
-    visibilityToggle?: boolean;
-  };
+  $props: AntdProps &
+    Omit<InputProps, 'type' | 'suffix'> & {
+      /**
+       * Whether show toggle button
+       * @default true
+       */
+      visibilityToggle?: boolean;
+    };
 }

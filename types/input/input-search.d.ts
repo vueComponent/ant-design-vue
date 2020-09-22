366811a99e0aa0735599c851e1cc45622becc997
John <John60676@qq.com>
2020-09-22 14:09:38 +0800

    fix(types): input prefix prop types

--- types/input/input-search.d.ts
+++ types/input/input-search.d.ts
@@ -2,24 +2,23 @@
 // Definitions by: akki-jat <https://github.com/akki-jat>
 // Definitions: https://github.com/vueComponent/ant-design-vue/types
 
-import { AntdComponent, AntdProps } from '../component';
+import { AntdComponent } from '../component';
 import { VNodeChild } from 'vue';
 import { InputProps } from './input';
 
 export declare class InputSearch extends AntdComponent {
-  $props: AntdProps &
-    InputProps & {
-      /**
-       * to show an enter button after input
-       * @default false
-       * @type any (boolean | slot)
-       */
-      enterButton?: boolean | VNodeChild | JSX.Element;
+  $props: InputProps & {
+    /**
+     * to show an enter button after input
+     * @default false
+     * @type any (boolean | slot)
+     */
+    enterButton?: boolean | VNodeChild | JSX.Element;
 
-      /**
-       * Callback when search is clicked or enter is pressed
-       * @type Function
-       */
-      onSearch?: (value: string | number, event: Event) => void;
-    };
+    /**
+     * Callback when search is clicked or enter is pressed
+     * @type Function
+     */
+    onSearch?: (value: string | number, event: Event) => void;
+  };
 }
--- types/input/password.d.ts
+++ types/input/password.d.ts
@@ -2,16 +2,15 @@
 // Definitions by: akki-jat <https://github.com/akki-jat>
 // Definitions: https://github.com/vueComponent/ant-design-vue/types
 
-import { AntdComponent, AntdProps } from '../component';
+import { AntdComponent } from '../component';
 import { InputProps } from './input';
 
 export declare class Password extends AntdComponent {
-  $props: AntdProps &
-    Omit<InputProps, 'type' | 'suffix'> & {
-      /**
-       * Whether show toggle button
-       * @default true
-       */
-      visibilityToggle?: boolean;
-    };
+  $props: Omit<InputProps, 'type' | 'suffix'> & {
+    /**
+     * Whether show toggle button
+     * @default true
+     */
+    visibilityToggle?: boolean;
+  };
 }

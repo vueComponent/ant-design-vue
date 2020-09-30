65a885f091c43ef8b1a980e62c7ae4982614bc1c
syt <ununian@outlook.com>
2020-09-30 16:28:56 +0800

    refactor(util): isFunction

--- components/_util/vue-types/utils.ts
+++ components/_util/vue-types/utils.ts
@@ -63,7 +63,8 @@ export const isArray =
  * @param {any} value - Value to check
  * @returns {boolean}
  */
-export const isFunction = (value: any) => toString.call(value) === '[object Function]';
+export const isFunction = (value: any): value is (...args: any[]) => any =>
+  toString.call(value) === '[object Function]';
 
 /**
  * Adds a `def` method to the object returning a new object with passed in argument as `default` property

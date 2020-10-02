f3c516619769e87b2076e1cba48ee6b924c1428f
Amour1688 <lcz_1996@foxmail.com>
2020-10-02 22:03:21 +0800

    chore: update demo

--- examples/App.tsx
+++ examples/App.tsx
@@ -1,20 +1,6 @@
 import Empty from '../components/empty';
 import '../components/empty/style';
 
-type TypeName<T> = T extends string
-  ? 'string'
-  : T extends number
-  ? 'number'
-  : T extends boolean
-  ? 'boolean'
-  : T extends undefined
-  ? 'undefined'
-  : T extends Function
-  ? 'function'
-  : 'object';
-
-type T10 = TypeName<string | (() => void)>;
-
 export default {
   render() {
     return <Empty />;

6ea812737f45b23867af191fa0bccbe26fd8756b
tangjinzhou <415800467@qq.com>
2020-10-03 17:16:34 +0800

    chore: update vue-types

---
+++ components/_util/vue-types/index.ts
@@ -0,0 +1,12 @@
+import { createTypes } from 'vue-types';
+const PropTypes = createTypes({
+  func: undefined,
+  bool: undefined,
+  string: undefined,
+  number: undefined,
+  array: undefined,
+  object: undefined,
+  integer: undefined,
+});
+
+export default PropTypes;

01096b556de5c63601f48fe7723120b13fc1cbcc
孙运天 <ununian@outlook.com>
2020-10-17 11:23:31 +0800

    refactor: statistic and countdown to ts (#2997)

--- components/statistic/index.ts
+++ components/statistic/index.ts
@@ -1,9 +1,10 @@
+import { App } from 'vue';
 import Statistic from './Statistic';
 import Countdown from './Countdown';
 
 Statistic.Countdown = Countdown;
 /* istanbul ignore next */
-Statistic.install = function(app) {
+Statistic.install = function(app: App) {
   app.component(Statistic.name, Statistic);
   app.component(Statistic.Countdown.name, Statistic.Countdown);
   return app;

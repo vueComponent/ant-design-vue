de7d58e528c8ae9f823a17d6896d25bfa2042fb4
cydiacen <binhao.cen@xiaobao100.com>
2020-10-27 11:23:07 +0800

    feat(color-picker): to typescript

---
+++ components/color-picker/index.ts
@@ -0,0 +1,8 @@
+import ColorPicker from './ColorPicker';
+/* istanbul ignore next */
+ColorPicker.install = function(app) {
+  app.component(ColorPicker.name, ColorPicker);
+  return app;
+};
+
+export default ColorPicker;

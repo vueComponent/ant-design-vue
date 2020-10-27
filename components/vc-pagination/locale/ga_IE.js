a43b128262c37951c105ed1999efbdff36eb86d2
tanjinzhou <415800467@qq.com>
2020-10-27 14:02:40 +0800

    feat: add ga_IE locale

---
+++ components/vc-pagination/locale/ga_IE.js
@@ -0,0 +1,15 @@
+export default {
+  // Options.jsx
+  items_per_page: '/ leathanach',
+  jump_to: 'Téigh',
+  jump_to_confirm: 'dheimhnigh',
+  page: '',
+
+  // Pagination.jsx
+  prev_page: 'Leathanach Roimhe Seo',
+  next_page: 'An chéad leathanach eile',
+  prev_5: '5 leathanach roimhe seo',
+  next_5: 'Ar Aghaidh 5 Leathanaigh',
+  prev_3: '3 leathanach roimhe seo',
+  next_3: 'Ar Aghaidh 3 Leathanaigh',
+};

82b805da13f073f739fa8af70ac004f28c9ae2f0
Govern Fu <frowhy@gmail.com>
2020-11-24 02:37:30 +0800

    chore: 添加演示代码

--- examples/App.vue
+++ examples/App.vue
@@ -1,23 +1,29 @@
 <template>
   <div>
-    <a-collapse :accordion="true" default-active-key="2" :bordered="false">
-      <a-collapse-panel key="1" header="This is panel header 1">
-        <p>{{ text }}</p>
-      </a-collapse-panel>
-      <a-collapse-panel key="2" header="This is panel header 2" :disabled="false">
-        <p>{{ text }}</p>
-      </a-collapse-panel>
-      <a-collapse-panel key="3" header="This is panel header 3">
-        <p>{{ text }}</p>
-      </a-collapse-panel>
-    </a-collapse>
+    <a-cascader :options="options"></a-cascader>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      text: `A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.`,
+      options: [
+        {
+          value: '0',
+          label: '0',
+          children: [
+            {
+              value: '0-0',
+              label: '0-0',
+              children: [
+                {
+                  value: '0-0-0',
+                  label: '0-0-0',
+                  children: [],
+                }],
+            }],
+        },
+      ],
     };
   },
 };

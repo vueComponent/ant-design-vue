8fb2c51a5dbbebe163e4be3f61c204acd21aae1b
John <John60676@qq.com>
2021-01-11 10:07:51 +0800

    chore: remove node-emoji
    fixes #3513

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,6 +1,3 @@
-/* eslint-disable max-len */
-const emoji = require('node-emoji');
-
 const env = process.env;
 const ADBLOCK = is(env.ADBLOCK);
 const CI = is(env.CI);
@@ -23,16 +20,11 @@ if (!ADBLOCK && !CI && !DISABLE_OPENCOLLECTIVE && !SILENT) {
   );
   log(
     '\u001B[96mThe project needs your help! Please consider supporting of ant-design-vue on Open Collective or Patreon: ' +
-      emoji.get('pray') +
       '\u001B[0m',
   );
   log('\u001B[96m>\u001B[94m https://opencollective.com/ant-design-vue \u001B[0m');
   log('\u001B[96m>\u001B[94m https://www.patreon.com/tangjinzhou \u001B[0m\n');
-  log(
-    '\u001B[96m更多赞助方式(支付宝、微信、Paypal)请查看如下链接: ' +
-      emoji.get('pray') +
-      '\u001B[0m',
-  );
+  log('\u001B[96m更多赞助方式(支付宝、微信、Paypal)请查看如下链接: ' + '\u001B[0m');
   log('\u001B[96m>\u001B[94m https://www.antdv.com/docs/vue/sponsor-cn \u001B[0m\n');
   log('\u001B[96mAnt Design Vue 官方网站: \u001B[0m');
   log('\u001B[96m>\u001B[94m https://www.antdv.com/ \u001B[0m\n');

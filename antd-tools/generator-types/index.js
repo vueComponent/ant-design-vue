9ad7a78f5d636756ac70efe06817085737d74d5e
Sendya <18x@loacg.com>
2021-03-05 23:35:57 +0800

    feat: add IDE types tips generator

---
+++ antd-tools/generator-types/index.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const pkg = require('../../package.json');
+const { parseAndWrite } = require('./lib/index.js');
+const rootPath = path.resolve(__dirname, '../../');
+
+try {
+  parseAndWrite({
+    version: pkg.version,
+    name: 'types',
+    path: path.resolve(rootPath, './v2-doc/src/docs'),
+    test: /zh-CN\.md/,
+    outputDir: path.resolve(rootPath, './vetur'),
+    tagPrefix: 'a-',
+  });
+  console.log('generator types success');
+} catch (e) {
+  console.error('generator types error', e);
+}
---
+++ antd-tools/generator-types/src/utils.ts
@@ -0,0 +1,30 @@
+// myName -> my-name
+export function toKebabCase(input: string): string {
+  return input.replace(/[A-Z]/g, (val, index) => (index === 0 ? '' : '-') + val.toLowerCase());
+}
+
+// name `v2.0.0` -> name
+export function removeVersion(str: string) {
+  return str.replace(/`(\w|\.)+`/g, '').trim();
+}
+
+// *boolean* -> boolean
+// _boolean_ -> boolean
+export function formatType(type: string) {
+  return type
+    .replace(/(^(\*|_))|((\*|_)$)/g, '')
+    .replace('\\', '')
+    .replace('\\', '');
+}
+
+export function getComponentName(name: string) {
+  let title = name
+    .split('-')
+    .map(it => it.substring(0, 1) + it.substring(1))
+    .join('');
+  return title.substring(0, 1).toUpperCase() + title.substring(1);
+}
+
+export function normalizePath(path: string): string {
+  return path.replace(/\\/g, '/');
+}

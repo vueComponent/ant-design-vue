eddbfecb27d197ac99046b7b3070e473629a1343
liuhang <1016817543@qq.com>
2021-03-31 14:50:59 +0800

    feat(table): 分组表头支持固定

--- components/table/ColumnGroup.jsx
+++ components/table/ColumnGroup.jsx
@@ -3,6 +3,7 @@ import PropTypes from '../_util/vue-types';
 export default {
   name: 'ATableColumnGroup',
   props: {
+    fixed: PropTypes.oneOfType([PropTypes.bool, PropTypes.oneOf(['left', 'right'])]),
     title: PropTypes.any,
   },
   __ANT_TABLE_COLUMN_GROUP: true,

e1dd34f858081cfaabc937835538bb6d7a106089
Mr小刘 <1016817543@qq.com>
2021-04-05 14:16:31 +0800

    feat: table column group support fixed（#3882） (#3884)

--- components/table/ColumnGroup.tsx
+++ components/table/ColumnGroup.tsx
@@ -1,9 +1,13 @@
 import { defineComponent } from 'vue';
-import PropTypes from '../_util/vue-types';
+import PropTypes, { withUndefined } from '../_util/vue-types';
+import { tuple } from '../_util/type';
 
 export default defineComponent({
   name: 'ATableColumnGroup',
   props: {
+    fixed: withUndefined(
+      PropTypes.oneOfType([PropTypes.looseBool, PropTypes.oneOf(tuple('left', 'right'))]),
+    ),
     title: PropTypes.any,
   },
   __ANT_TABLE_COLUMN_GROUP: true,

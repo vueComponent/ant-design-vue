b96fc440e923cf200f7d32ec773c49e44b736b9a
tangjinzhou <415800467@qq.com>
2021-05-11 22:42:14 +0800

    refactor: backtop

--- components/back-top/index.tsx
+++ components/back-top/index.tsx
@@ -7,8 +7,13 @@ import {
   onBeforeUnmount,
   onMounted,
   reactive,
+  PropType,
+  ref,
+  watch,
+  onDeactivated,
+  computed,
 } from 'vue';
-import classNames from '../_util/classNames';
+import VerticalAlignTopOutlined from '@ant-design/icons-vue/VerticalAlignTopOutlined';
 import PropTypes from '../_util/vue-types';
 import addEventListener from '../vc-util/Dom/addEventListener';
 import getScroll from '../_util/getScroll';
@@ -16,18 +21,12 @@ import { getTransitionProps, Transition } from '../_util/transition';
 import { defaultConfigProvider } from '../config-provider';
 import scrollTo from '../_util/scrollTo';
 import { withInstall } from '../_util/type';
-
-function getDefaultTarget() {
-  return window;
-}
+import throttleByAnimationFrame from '../_util/throttleByAnimationFrame';
 
 export const backTopProps = {
-  // 滚动高度达到此参数值才出现 BackTop
   visibilityHeight: PropTypes.number.def(400),
-  // 回到顶部所需时间（ms） @4.4.0
   duration: PropTypes.number.def(450),
-  // 设置需要监听其滚动事件的元素，值为一个返回对应 DOM 元素的函数
-  target: PropTypes.func,
+  target: Function as PropType<() => HTMLElement | Window | Document>,
   prefixCls: PropTypes.string,
   onClick: PropTypes.func,
   // visible: PropTypes.looseBool, // Only for test. Don't use it.
@@ -42,11 +41,14 @@ const BackTop = defineComponent({
   emits: ['click'],
   setup(props, { slots, attrs, emit }) {
     const configProvider = inject('configProvider', defaultConfigProvider);
-
+    const domRef = ref();
     const state = reactive({
       visible: false,
       scrollEvent: null,
     });
+
+    const getDefaultTarget = () =>
+      domRef.value && domRef.value.ownerDocument ? domRef.value.ownerDocument : window;
 
     const scrollToTop = (e: Event) => {
       const { target = getDefaultTarget, duration } = props;
@@ -57,51 +59,85 @@ const BackTop = defineComponent({
       emit('click', e);
     };
 
-    const handleScroll = () => {
-      const { visibilityHeight, target = getDefaultTarget } = props;
-      const scrollTop = getScroll(target(), true);
+    const handleScroll = throttleByAnimationFrame((e: Event | { target: any }) => {
+      const { visibilityHeight } = props;
+      const scrollTop = getScroll(e.target, true);
       state.visible = scrollTop > visibilityHeight;
+    });
+
+    const bindScrollEvent = () => {
+      const { target } = props;
+      const getTarget = target || getDefaultTarget;
+      const container = getTarget();
+      state.scrollEvent = addEventListener(container, 'scroll', (e: Event) => {
+        handleScroll(e);
+      });
+      handleScroll({
+        target: container,
+      });
     };
+
+    const scrollRemove = () => {
+      if (state.scrollEvent) {
+        state.scrollEvent.remove();
+      }
+      (handleScroll as any).cancel();
+    };
+
+    watch(
+      () => props.target,
+      () => {
+        scrollRemove();
+        nextTick(() => {
+          bindScrollEvent();
+        });
+      },
+    );
 
     onMounted(() => {
       nextTick(() => {
-        const getTarget = props.target || getDefaultTarget;
-        state.scrollEvent = addEventListener(getTarget(), 'scroll', handleScroll);
-        handleScroll();
+        bindScrollEvent();
       });
     });
 
     onActivated(() => {
       nextTick(() => {
-        handleScroll();
+        bindScrollEvent();
       });
     });
 
-    onBeforeUnmount(() => {
-      if (state.scrollEvent) {
-        state.scrollEvent.remove();
-      }
+    onDeactivated(() => {
+      scrollRemove();
     });
 
-    return () => {
-      const { prefixCls: customizePrefixCls } = props;
+    onBeforeUnmount(() => {
+      scrollRemove();
+    });
 
-      const getPrefixCls = configProvider.getPrefixCls;
-      const prefixCls = getPrefixCls('back-top', customizePrefixCls);
-      const classString = classNames(prefixCls, attrs.class);
+    const prefixCls = computed(() => configProvider.getPrefixCls('back-top', props.prefixCls));
+
+    return () => {
       const defaultElement = (
-        <div class={`${prefixCls}-content`}>
-          <div class={`${prefixCls}-icon`} />
+        <div class={`${prefixCls.value}-content`}>
+          <div class={`${prefixCls.value}-icon`}>
+            <VerticalAlignTopOutlined />
+          </div>
         </div>
       );
       const divProps = {
         ...attrs,
         onClick: scrollToTop,
-        class: classString,
+        class: {
+          [`${prefixCls.value}`]: true,
+          [`${attrs.class}`]: attrs.class,
+          [`${prefixCls}-rtl`]: configProvider.direction === 'rtl',
+        },
       };
 
       const backTopBtn = state.visible ? (
-        <div {...divProps}>{slots.default?.() || defaultElement}</div>
+        <div {...divProps} ref={domRef}>
+          {slots.default?.() || defaultElement}
+        </div>
       ) : null;
       const transitionProps = getTransitionProps('fade');
       return <Transition {...transitionProps}>{backTopBtn}</Transition>;

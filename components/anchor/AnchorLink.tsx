1877d66cc529e6d0b3c8d65923327ce0c2cfc547
言肆 <18x@loacg.com>
2021-05-24 16:42:54 +0800

    refactor(Anchor): use composition api (#4054)

--- components/anchor/AnchorLink.tsx
+++ components/anchor/AnchorLink.tsx
@@ -1,6 +1,14 @@
-import { ComponentPublicInstance, defineComponent, inject, nextTick } from 'vue';
+import {
+  ComponentInternalInstance,
+  defineComponent,
+  inject,
+  nextTick,
+  onBeforeUnmount,
+  onMounted,
+  watch,
+} from 'vue';
 import PropTypes from '../_util/vue-types';
-import { getComponent } from '../_util/props-util';
+import { getPropsSlot } from '../_util/props-util';
 import classNames from '../_util/classNames';
 import { defaultConfigProvider } from '../config-provider';
 import { AntAnchor } from './Anchor';
@@ -18,72 +26,72 @@ const AnchorLinkProps = {
 export default defineComponent({
   name: 'AAnchorLink',
   props: AnchorLinkProps,
-  setup() {
-    return {
-      antAnchor: inject('antAnchor', {
-        registerLink: noop,
-        unregisterLink: noop,
-        scrollTo: noop,
-        $data: {},
-      } as AntAnchor),
-      antAnchorContext: inject('antAnchorContext', {}) as ComponentPublicInstance,
-      configProvider: inject('configProvider', defaultConfigProvider),
-    };
-  },
-  watch: {
-    href(val, oldVal) {
-      nextTick(() => {
-        this.antAnchor.unregisterLink(oldVal);
-        this.antAnchor.registerLink(val);
-      });
-    },
-  },
+  setup(props, { slots }) {
+    const antAnchor = inject('antAnchor', {
+      registerLink: noop,
+      unregisterLink: noop,
+      scrollTo: noop,
+      $data: {},
+    } as AntAnchor);
+    const antAnchorContext = inject('antAnchorContext', {}) as ComponentInternalInstance;
+    const configProvider = inject('configProvider', defaultConfigProvider);
 
-  mounted() {
-    this.antAnchor.registerLink(this.href);
-  },
-
-  beforeUnmount() {
-    this.antAnchor.unregisterLink(this.href);
-  },
-  methods: {
-    handleClick(e: Event) {
-      this.antAnchor.scrollTo(this.href);
-      const { scrollTo } = this.antAnchor;
-      const { href, title } = this.$props;
-      if (this.antAnchorContext.$emit) {
-        this.antAnchorContext.$emit('click', e, { title, href });
+    const handleClick = (e: Event) => {
+      // antAnchor.scrollTo(props.href);
+      const { scrollTo } = antAnchor;
+      const { href, title } = props;
+      if (antAnchorContext.emit) {
+        antAnchorContext.emit('click', e, { title, href });
       }
       scrollTo(href);
-    },
-  },
-  render() {
-    const { prefixCls: customizePrefixCls, href, $slots, target } = this;
+    };
 
-    const getPrefixCls = this.configProvider.getPrefixCls;
-    const prefixCls = getPrefixCls('anchor', customizePrefixCls);
-
-    const title = getComponent(this, 'title');
-    const active = this.antAnchor.$data.activeLink === href;
-    const wrapperClassName = classNames(`${prefixCls}-link`, {
-      [`${prefixCls}-link-active`]: active,
-    });
-    const titleClassName = classNames(`${prefixCls}-link-title`, {
-      [`${prefixCls}-link-title-active`]: active,
-    });
-    return (
-      <div class={wrapperClassName}>
-        <a
-          class={titleClassName}
-          href={href}
-          title={typeof title === 'string' ? title : ''}
-          target={target}
-          onClick={this.handleClick}
-        >
-          {title}
-        </a>
-        {$slots.default?.()}
-      </div>
+    watch(
+      () => props.href,
+      (val, oldVal) => {
+        nextTick(() => {
+          antAnchor.unregisterLink(oldVal);
+          antAnchor.registerLink(val);
+        });
+      },
     );
+
+    onMounted(() => {
+      antAnchor.registerLink(props.href);
+    });
+
+    onBeforeUnmount(() => {
+      antAnchor.unregisterLink(props.href);
+    });
+
+    return () => {
+      const { prefixCls: customizePrefixCls, href, target } = props;
+
+      const getPrefixCls = configProvider.getPrefixCls;
+      const prefixCls = getPrefixCls('anchor', customizePrefixCls);
+
+      const title = getPropsSlot(slots, props, 'title');
+      const active = antAnchor.$data.activeLink === href;
+      const wrapperClassName = classNames(`${prefixCls}-link`, {
+        [`${prefixCls}-link-active`]: active,
+      });
+      const titleClassName = classNames(`${prefixCls}-link-title`, {
+        [`${prefixCls}-link-title-active`]: active,
+      });
+      return (
+        <div class={wrapperClassName}>
+          <a
+            class={titleClassName}
+            href={href}
+            title={typeof title === 'string' ? title : ''}
+            target={target}
+            onClick={handleClick}
+          >
+            {title}
+          </a>
+          {slots.default?.()}
+        </div>
+      );
+    };
   },
 });

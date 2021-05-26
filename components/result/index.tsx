3b89f59094247fbe7b8a98ab0fe7864ee20cb429
tanjinzhou <415800467@qq.com>
2021-05-26 18:55:32 +0800

    refactor: result

--- components/result/index.tsx
+++ components/result/index.tsx
@@ -1,8 +1,6 @@
-import { App, defineComponent, inject, VNodeTypes, Plugin } from 'vue';
+import { App, defineComponent, VNodeTypes, Plugin, ExtractPropTypes, computed } from 'vue';
 import PropTypes from '../_util/vue-types';
 import { tuple } from '../_util/type';
-import { getComponent } from '../_util/props-util';
-import { defaultConfigProvider } from '../config-provider';
 import CheckCircleFilled from '@ant-design/icons-vue/CheckCircleFilled';
 import CloseCircleFilled from '@ant-design/icons-vue/CloseCircleFilled';
 import ExclamationCircleFilled from '@ant-design/icons-vue/ExclamationCircleFilled';
@@ -10,6 +8,8 @@ import WarningFilled from '@ant-design/icons-vue/WarningFilled';
 import noFound from './noFound';
 import serverError from './serverError';
 import unauthorized from './unauthorized';
+import useConfigInject from '../_util/hooks/useConfigInject';
+import classNames from '../_util/classNames';
 
 export const IconMap = {
   success: CheckCircleFilled,
@@ -27,7 +27,7 @@ export const ExceptionMap = {
 // ExceptionImageMap keys
 const ExceptionStatus = Object.keys(ExceptionMap);
 
-export const ResultProps = {
+export const resultProps = {
   prefixCls: PropTypes.string,
   icon: PropTypes.any,
   status: PropTypes.oneOf(tuple('success', 'error', 'info', 'warning', '404', '403', '500')).def(
@@ -37,6 +37,8 @@ export const ResultProps = {
   subTitle: PropTypes.any,
   extra: PropTypes.any,
 };
+
+export type ResultProps = Partial<ExtractPropTypes<typeof resultProps>>;
 
 const renderIcon = (prefixCls: string, { status, icon }) => {
   if (ExceptionStatus.includes(`${status}`)) {
@@ -57,31 +59,31 @@ const renderExtra = (prefixCls: string, extra: VNodeTypes) =>
 
 const Result = defineComponent({
   name: 'AResult',
-  props: ResultProps,
-  setup() {
-    return {
-      configProvider: inject('configProvider', defaultConfigProvider),
-    };
-  },
-  render() {
-    const { prefixCls: customizePrefixCls, status } = this;
-    const getPrefixCls = this.configProvider.getPrefixCls;
-    const prefixCls = getPrefixCls('result', customizePrefixCls);
-
-    const title = getComponent(this, 'title');
-    const subTitle = getComponent(this, 'subTitle');
-    const icon = getComponent(this, 'icon');
-    const extra = getComponent(this, 'extra');
-
-    return (
-      <div class={`${prefixCls} ${prefixCls}-${status}`}>
-        {renderIcon(prefixCls, { status, icon })}
-        <div class={`${prefixCls}-title`}>{title}</div>
-        {subTitle && <div class={`${prefixCls}-subtitle`}>{subTitle}</div>}
-        {this.$slots.default && <div class={`${prefixCls}-content`}>{this.$slots.default()}</div>}
-        {renderExtra(prefixCls, extra)}
-      </div>
+  props: resultProps,
+  slots: ['title', 'subTitle', 'icon', 'extra'],
+  setup(props, { slots }) {
+    const { prefixCls, direction } = useConfigInject('result', props);
+    const className = computed(() =>
+      classNames(prefixCls.value, `${prefixCls.value}-${props.status}`, {
+        [`${prefixCls.value}-rtl`]: direction.value === 'rtl',
+      }),
     );
+    return () => {
+      const title = props.title ?? slots.title?.();
+      const subTitle = props.subTitle ?? slots.subTitle?.();
+      const icon = props.icon ?? slots.icon?.();
+      const extra = props.extra ?? slots.extra?.();
+      const pre = prefixCls.value;
+      return (
+        <div class={className.value}>
+          {renderIcon(pre, { status: props.status, icon })}
+          <div class={`${pre}-title`}>{title}</div>
+          {subTitle && <div class={`${pre}-subtitle`}>{subTitle}</div>}
+          {renderExtra(pre, extra)}
+          {slots.default && <div class={`${pre}-content`}>{slots.default()}</div>}
+        </div>
+      );
+    };
   },
 });
 

f7908fd24b8af48ceb9c2fd22c57d2c3c29aff32
ajuner <106791576@qq.com>
2021-06-25 10:53:09 +0800

    refactor(tooltip): use composition api

--- components/tooltip/abstractTooltipProps.ts
+++ components/tooltip/abstractTooltipProps.ts
@@ -1,27 +1,30 @@
 import PropTypes from '../_util/vue-types';
 import { tuple } from '../_util/type';
-const triggerType = PropTypes.oneOf(tuple('hover', 'focus', 'click', 'contextmenu'));
+export const triggerTypes = tuple('hover', 'focus', 'click', 'contextmenu');
+
+export const placementTypes = tuple(
+  'top',
+  'left',
+  'right',
+  'bottom',
+  'topLeft',
+  'topRight',
+  'bottomLeft',
+  'bottomRight',
+  'leftTop',
+  'leftBottom',
+  'rightTop',
+  'rightBottom',
+);
 
 export default () => ({
-  trigger: PropTypes.oneOfType([triggerType, PropTypes.arrayOf(triggerType)]).def('hover'),
+  trigger: PropTypes.oneOfType([
+    PropTypes.oneOf(triggerTypes),
+    PropTypes.arrayOf(PropTypes.oneOf(triggerTypes)),
+  ]).def('hover'),
   visible: PropTypes.looseBool,
-  defaultVisible: PropTypes.looseBool,
-  placement: PropTypes.oneOf(
-    tuple(
-      'top',
-      'left',
-      'right',
-      'bottom',
-      'topLeft',
-      'topRight',
-      'bottomLeft',
-      'bottomRight',
-      'leftTop',
-      'leftBottom',
-      'rightTop',
-      'rightBottom',
-    ),
-  ).def('top'),
+  // defaultVisible: PropTypes.looseBool,
+  placement: PropTypes.oneOf(placementTypes).def('top'),
   color: PropTypes.string,
   transitionName: PropTypes.string.def('zoom-big-fast'),
   overlayStyle: PropTypes.object.def(() => ({})),

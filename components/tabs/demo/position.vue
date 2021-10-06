488437b819a5782b5fb9dea952fe5db44948031e
tangjinzhou <415800467@qq.com>
2021-10-06 22:15:14 +0800

    doc: update tabs demo

--- components/tabs/demo/position.vue
+++ components/tabs/demo/position.vue
@@ -8,28 +8,26 @@ title:
 
 ## zh-CN
 
-有四个位置，`tabPosition="left|right|top|bottom"`。
+有四个位置，`tabPosition="left|right|top|bottom"`。在移动端下，`bottom|right` 会自动切换成 `top`。
 
 ## en-US
 
-Tab's position: left, right, top or bottom.
+Tab's position: left, right, top or bottom.  Will auto switch to `top` in mobile.
 
 </docs>
 
 <template>
-  <div style="width: 500px">
-    <a-radio-group v-model:value="tabPosition" style="margin: 8px">
-      <a-radio-button value="top">top</a-radio-button>
-      <a-radio-button value="bottom">bottom</a-radio-button>
-      <a-radio-button value="left">left</a-radio-button>
-      <a-radio-button value="right">right</a-radio-button>
-    </a-radio-group>
-    <a-tabs v-model:activeKey="activeKey" :tab-position="tabPosition">
-      <a-tab-pane key="1" tab="Tab 1">Content of Tab 1</a-tab-pane>
-      <a-tab-pane key="2" tab="Tab 2">Content of Tab 2</a-tab-pane>
-      <a-tab-pane key="3" tab="Tab 3">Content of Tab 3</a-tab-pane>
-    </a-tabs>
-  </div>
+  <a-radio-group v-model:value="tabPosition" style="margin: 8px">
+    <a-radio-button value="top">top</a-radio-button>
+    <a-radio-button value="bottom">bottom</a-radio-button>
+    <a-radio-button value="left">left</a-radio-button>
+    <a-radio-button value="right">right</a-radio-button>
+  </a-radio-group>
+  <a-tabs v-model:activeKey="activeKey" :tab-position="tabPosition">
+    <a-tab-pane key="1" tab="Tab 1">Content of Tab 1</a-tab-pane>
+    <a-tab-pane key="2" tab="Tab 2">Content of Tab 2</a-tab-pane>
+    <a-tab-pane key="3" tab="Tab 3">Content of Tab 3</a-tab-pane>
+  </a-tabs>
 </template>
 <script lang="ts">
 import { defineComponent, ref } from 'vue';

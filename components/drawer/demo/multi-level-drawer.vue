cc0a53c191b6d37867449246a06b8a9b69ca03a6
tangjinzhou <415800467@qq.com>
2021-10-07 22:26:31 +0800

    doc: udpate drawer demo

--- components/drawer/demo/multi-level-drawer.vue
+++ components/drawer/demo/multi-level-drawer.vue
@@ -16,24 +16,6 @@ Open a new drawer on top of an existing drawer to handle multi branch tasks.
 
 </docs>
 
-<docs>
----
-order: 6
-title:
-  zh-CN: 多层抽屉
-  en-US: Multi-level drawer
----
-
-## zh-CN
-
-在抽屉内打开新的抽屉，用以解决多分支任务的复杂状况。
-
-## en-US
-
-Open a new drawer on top of an existing drawer to handle multi branch tasks.
-
-</docs>
-
 <template>
   <a-button type="primary" @click="showDrawer">Open</a-button>
   <a-drawer

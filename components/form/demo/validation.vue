215a4d575201add4f6c32a17c3a861f4ccceff9f
tangjinzhou <415800467@qq.com>
2021-11-05 09:41:56 +0800

    doc: update form typos, close #4850

--- components/form/demo/validation.vue
+++ components/form/demo/validation.vue
@@ -8,11 +8,11 @@ title:
 
 ## zh-CN
 
-Form 组件提供了表单验证的功能，只需要通过 `rules` 属性传入约定的验证规则，并将 `FormItem` 的 `prop` 属性设置为需校验的字段名即可。校验规则参见 [async-validator](https://github.com/yiminghe/async-validator)
+Form 组件提供了表单验证的功能，只需要通过 `rules` 属性传入约定的验证规则，并将 `FormItem` 的 `name` 属性设置为需校验的字段名即可。校验规则参见 [async-validator](https://github.com/yiminghe/async-validator)
 
 ## en-US
 
-Just add the `rules` attribute for `Form` component, pass validation rules, and set `prop` attribute for `FormItem` as a specific key that needs to be validated. See more information at [async-validator](https://github.com/yiminghe/async-validator).
+Just add the `rules` attribute for `Form` component, pass validation rules, and set `name` attribute for `FormItem` as a specific key that needs to be validated. See more information at [async-validator](https://github.com/yiminghe/async-validator).
 </docs>
 
 <template>

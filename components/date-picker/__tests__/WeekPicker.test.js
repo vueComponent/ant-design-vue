3d3dcd5c449a2ff3c77021af5bc4a43c3a6ecd35
tangjinzhou <415800467@qq.com>
2022-01-10 10:22:55 +0800

    test: update snap

--- components/date-picker/__tests__/WeekPicker.test.js
+++ components/date-picker/__tests__/WeekPicker.test.js
@@ -1,11 +1,14 @@
 import { mount } from '@vue/test-utils';
-import { asyncExpect } from '../../../tests/utils';
+import { asyncExpect, setMockDate } from '../../../tests/utils';
 import DatePicker from '..';
 import focusTest from '../../../tests/shared/focusTest';
 
 const { WeekPicker } = DatePicker;
 jest.mock('../../_util/Portal');
 describe('WeekPicker', () => {
+  beforeEach(() => {
+    setMockDate();
+  });
   focusTest(WeekPicker);
 
   it('should support style prop', async () => {

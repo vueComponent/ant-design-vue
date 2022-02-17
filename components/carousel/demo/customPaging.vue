2e0bfd1f018ba9dea2b78a50ad373ee7cd2e0c35
tangjinzhou <415800467@qq.com>
2022-02-17 21:02:21 +0800

    doc: add carouse img

--- components/carousel/demo/customPaging.vue
+++ components/carousel/demo/customPaging.vue
@@ -32,7 +32,7 @@ Custom paging display
 import { defineComponent } from 'vue';
 
 const baseUrl =
-  'https://raw.githubusercontent.com/vueComponent/ant-design-vue/next/components/vc-slick/assets/img/react-slick/';
+  'https://raw.githubusercontent.com/vueComponent/ant-design-vue/next/components/carousel/demo/';
 export default defineComponent({
   setup() {
     const getImgUrl = (i: number) => {

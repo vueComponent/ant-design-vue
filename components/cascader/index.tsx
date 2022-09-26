baa18ef5b3f5da333d860a5cbd6eed23aba60195
tangjinzhou <415800467@qq.com>
2022-09-26 22:04:25 +0800

    Update index.tsx

--- components/cascader/index.tsx
+++ components/cascader/index.tsx
@@ -136,7 +136,7 @@ const Cascader = defineComponent({
     if (process.env.NODE_ENV !== 'production') {
       watchEffect(() => {
         devWarning(
-          !props.multiple ? !props.displayRender || !slots.displayRender : !slots.tagRender,
+          !props.multiple || !props.displayRender || !slots.displayRender,
           'Cascader',
           '`displayRender` not work on `multiple`. Please use `tagRender` instead.',
         );
@@ -280,7 +280,7 @@ const Cascader = defineComponent({
             ...slots,
             checkable: () => <span class={`${cascaderPrefixCls.value}-checkbox-inner`} />,
           }}
-          tagRender={slots.tagRender}
+          tagRender={props.tagRender || slots.tagRender}
           displayRender={props.displayRender || slots.displayRender}
           maxTagPlaceholder={props.maxTagPlaceholder || slots.maxTagPlaceholder}
           showArrow={props.showArrow}

fc0f0d8a966257b43dbe57e23aefa44ebf57ca69
bqy_fe <1743369777@qq.com>
2023-02-07 09:57:31 +0800

    refactor(empty): less to cssinjs (#6230)
    * refactor(empty): less to cssinjs
    
    * chore: remove unuse code
    
    * fix: reactivity lose

--- components/empty/index.tsx
+++ components/empty/index.tsx
@@ -1,13 +1,15 @@
-import type { CSSProperties, FunctionalComponent, PropType } from 'vue';
+import { defineComponent } from 'vue';
+import type { CSSProperties, PropType, ExtractPropTypes } from 'vue';
 import classNames from '../_util/classNames';
 import LocaleReceiver from '../locale-provider/LocaleReceiver';
 import DefaultEmptyImg from './empty';
 import SimpleEmptyImg from './simple';
 import { filterEmpty } from '../_util/props-util';
 import PropTypes from '../_util/vue-types';
-import type { VueNode } from '../_util/type';
 import { withInstall } from '../_util/type';
 import useConfigInject from '../config-provider/hooks/useConfigInject';
+
+import useStyle from './style';
 
 const defaultEmptyImg = <DefaultEmptyImg />;
 const simpleEmptyImg = <SimpleEmptyImg />;
@@ -16,70 +18,73 @@ interface Locale {
   description?: string;
 }
 
-export interface EmptyProps {
-  prefixCls?: string;
-  class?: any;
-  style?: string | CSSProperties;
-  imageStyle?: CSSProperties;
-  image?: VueNode | null;
-  description?: VueNode;
-}
+export const emptyProps = () => ({
+  prefixCls: String,
+  class: PropTypes.any,
+  style: [String, Object] as PropType<string | CSSProperties>,
+  imageStyle: Object as PropType<CSSProperties>,
+  image: PropTypes.any,
+  description: PropTypes.any,
+});
 
-interface EmptyType extends FunctionalComponent<EmptyProps> {
-  displayName: string;
-  PRESENTED_IMAGE_DEFAULT: VueNode;
-  PRESENTED_IMAGE_SIMPLE: VueNode;
-}
+export type EmptyProps = Partial<ExtractPropTypes<ReturnType<typeof emptyProps>>>;
 
-const Empty: EmptyType = (props, { slots = {}, attrs }) => {
-  const { direction, prefixCls: prefixClsRef } = useConfigInject('empty', props);
-  const prefixCls = prefixClsRef.value;
+const Empty = defineComponent({
+  name: 'AEmpty',
+  inheritAttrs: false,
+  props: emptyProps(),
+  setup(props, { slots = {}, attrs }) {
+    const { direction, prefixCls: prefixClsRef } = useConfigInject('empty', props);
 
-  const {
-    image = defaultEmptyImg,
-    description = slots.description?.() || undefined,
-    imageStyle,
-    class: className = '',
-    ...restProps
-  } = { ...props, ...attrs };
+    const [wrapSSR, hashId] = useStyle(prefixClsRef);
 
-  return (
-    <LocaleReceiver
-      componentName="Empty"
-      children={(locale: Locale) => {
-        const des = typeof description !== 'undefined' ? description : locale.description;
-        const alt = typeof des === 'string' ? des : 'empty';
-        let imageNode: EmptyProps['image'] = null;
+    return () => {
+      const prefixCls = prefixClsRef.value;
+      const {
+        image = defaultEmptyImg,
+        description = slots.description?.() || undefined,
+        imageStyle,
+        class: className = '',
+        ...restProps
+      } = { ...props, ...attrs };
 
-        if (typeof image === 'string') {
-          imageNode = <img alt={alt} src={image} />;
-        } else {
-          imageNode = image;
-        }
+      return wrapSSR(
+        <LocaleReceiver
+          componentName="Empty"
+          children={(locale: Locale) => {
+            const des = typeof description !== 'undefined' ? description : locale.description;
+            const alt = typeof des === 'string' ? des : 'empty';
+            let imageNode: EmptyProps['image'] = null;
 
-        return (
-          <div
-            class={classNames(prefixCls, className, {
-              [`${prefixCls}-normal`]: image === simpleEmptyImg,
-              [`${prefixCls}-rtl`]: direction.value === 'rtl',
-            })}
-            {...restProps}
-          >
-            <div class={`${prefixCls}-image`} style={imageStyle}>
-              {imageNode}
-            </div>
-            {des && <p class={`${prefixCls}-description`}>{des}</p>}
-            {slots.default && (
-              <div class={`${prefixCls}-footer`}>{filterEmpty(slots.default())}</div>
-            )}
-          </div>
-        );
-      }}
-    />
-  );
-};
+            if (typeof image === 'string') {
+              imageNode = <img alt={alt} src={image} />;
+            } else {
+              imageNode = image;
+            }
 
-Empty.displayName = 'AEmpty';
+            return (
+              <div
+                class={classNames(prefixCls, className, hashId.value, {
+                  [`${prefixCls}-normal`]: image === simpleEmptyImg,
+                  [`${prefixCls}-rtl`]: direction.value === 'rtl',
+                })}
+                {...restProps}
+              >
+                <div class={`${prefixCls}-image`} style={imageStyle}>
+                  {imageNode}
+                </div>
+                {des && <p class={`${prefixCls}-description`}>{des}</p>}
+                {slots.default && (
+                  <div class={`${prefixCls}-footer`}>{filterEmpty(slots.default())}</div>
+                )}
+              </div>
+            );
+          }}
+        />,
+      );
+    };
+  },
+});
 
 Empty.PRESENTED_IMAGE_DEFAULT = defaultEmptyImg;
 Empty.PRESENTED_IMAGE_SIMPLE = simpleEmptyImg;

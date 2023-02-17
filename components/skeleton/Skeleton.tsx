56a7cdd629799eaba5344e1b6fdfb50dad77a83e
shifeng1993 <shifeng199307@gmail.com>
2023-02-17 20:51:37 +0800

    fix string type

--- components/skeleton/Skeleton.tsx
+++ components/skeleton/Skeleton.tsx
@@ -96,9 +96,9 @@ const Skeleton = defineComponent({
       const { loading, avatar, title, paragraph, active, round } = props;
       const pre = prefixCls.value;
       if (loading || props.loading === undefined) {
-        const hasAvatar = !!avatar;
-        const hasTitle = !!title;
-        const hasParagraph = !!paragraph;
+        const hasAvatar = !!avatar || (avatar as string) === '';
+        const hasTitle = !!title || (title as string) === '';
+        const hasParagraph = !!paragraph || (paragraph as string) === '';
 
         // Avatar
         let avatarNode;

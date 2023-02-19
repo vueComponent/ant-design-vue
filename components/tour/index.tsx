b8431a17d9c158e403289ec2e73de004fa50ba07
yang_xiaodong <yacoda@qq.com>
2023-02-19 12:20:13 +0800

    feat: update Tour ts

--- components/tour/index.tsx
+++ components/tour/index.tsx
@@ -1,6 +1,7 @@
 import { withInstall } from '../_util/type';
-import Tour, { TourProps } from './Tour';
-import { TourStepInfo } from './TourStep';
+import Tour from './Tour';
+import type { TourProps } from './Tour';
+import type { TourStepInfo } from './TourStep';
 
 export { TourProps, TourStepInfo };
 export default withInstall(Tour);

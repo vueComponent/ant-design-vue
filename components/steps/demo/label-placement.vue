839a60ee96a64f48df961b342c88e524bb7ad5ef
CCherry07 <2405693142@qq.com>
2023-03-29 15:34:14 +0800

    feat(steps): add Label Placement and Inline Steps demo

---
+++ components/steps/demo/label-placement.vue
@@ -0,0 +1,51 @@
+<docs>
+---
+order: 5
+title:
+  zh-CN: 标签放置位置
+  en-US: Label Placement
+---
+
+## zh-CN
+
+修改标签放置位置为 `vertical`。
+
+## en-US
+
+Set labelPlacement to `vertical`.
+
+</docs>
+<template>
+  <div>
+    <a-steps :current="1" label-placement="vertical" :items="items" />
+    <br />
+    <a-steps :current="1" :percent="60" label-placement="vertical" :items="items" />
+    <br />
+    <a-steps :current="1" size="small" label-placement="vertical" :items="items" />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const items = ref([
+      {
+        title: 'Finished',
+        description: 'This is a description.',
+      },
+      {
+        title: 'In Progress',
+        description: 'This is a description.',
+      },
+      {
+        title: 'Waiting',
+        description: 'This is a description.',
+      },
+    ]);
+    return {
+      items,
+    };
+  },
+});
+</script>

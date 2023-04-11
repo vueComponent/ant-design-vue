4a02bd69275079539e76f6de97d6a422790ce1f8
CCherry07 <2405693142@qq.com>
2023-04-11 17:31:36 +0800

    feat(anchor): update demo

---
+++ components/anchor/demo/horizontal.vue
@@ -0,0 +1,64 @@
+<docs>
+---
+order: 1
+title:
+  zh-CN: 横向 Anchor。
+  en-US: Horizontally aligned anchors
+---
+
+
+## zh-CN
+
+横向 Anchor。
+
+## en-US
+
+Horizontally aligned anchors
+
+</docs>
+
+<template>
+  <div
+    style="
+       {
+        padding: '20px';
+      }
+    "
+  >
+    <a-anchor
+      direction="horizontal"
+      :items="[
+        {
+          key: 'horizontally-part-1',
+          href: '#horizontally-part-1',
+          title: 'Part 1',
+        },
+        {
+          key: 'horizontally-part-2',
+          href: '#horizontally-part-2',
+          title: 'Part 2',
+        },
+        {
+          key: 'horizontally-part-3',
+          href: '#horizontally-part-3',
+          title: 'Part 3',
+        },
+        {
+          key: 'horizontally-part-4',
+          href: '#horizontally-part-4',
+          title: 'Part 4',
+        },
+        {
+          key: 'horizontally-part-5',
+          href: '#horizontally-part-5',
+          title: 'Part 5',
+        },
+        {
+          key: 'horizontally-part-6',
+          href: '#horizontally-part-6',
+          title: 'Part 6',
+        },
+      ]"
+    />
+  </div>
+</template>

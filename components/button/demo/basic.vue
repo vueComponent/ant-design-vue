9fdc297686557ca87f3a3cb79a1497a577926797
Cherry7 <79909910+CCherry07@users.noreply.github.com>
2023-05-07 20:33:24 +0800

    docs(button): update demo with space (#6536)
    * feat(button): demo space
    
    * test(button): update demo snap
    
    * chore(button): disabled demo Ghost space
    
    * test(button): update disabled demo snap

--- components/button/demo/basic.vue
+++ components/button/demo/basic.vue
@@ -17,9 +17,11 @@ There are `primary` button, `default` button, `dashed` button, `text` button and
 </docs>
 
 <template>
-  <a-button type="primary">Primary Button</a-button>
-  <a-button>Default Button</a-button>
-  <a-button type="dashed">Dashed Button</a-button>
-  <a-button type="text">Text Button</a-button>
-  <a-button type="link">Link Button</a-button>
+  <a-space wrap>
+    <a-button type="primary">Primary Button</a-button>
+    <a-button>Default Button</a-button>
+    <a-button type="dashed">Dashed Button</a-button>
+    <a-button type="text">Text Button</a-button>
+    <a-button type="link">Link Button</a-button>
+  </a-space>
 </template>

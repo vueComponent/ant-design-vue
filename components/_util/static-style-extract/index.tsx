d360f0f03e056e46145f21307e984d19d220055e
aibayanyu <aibayanyu@qq.com>
2023-07-01 11:35:18 +0800

    feat: add static-style-extract

---
+++ components/_util/static-style-extract/index.tsx
@@ -0,0 +1,46 @@
+import { createCache, extractStyle as extStyle, StyleProvider } from '../cssinjs';
+import * as antd from '../../components';
+import { renderToString } from 'vue/server-renderer';
+import type { CustomRender } from './interface';
+const blackList: string[] = [
+  'ConfigProvider',
+  'Drawer',
+  'Grid',
+  'Modal',
+  'Popconfirm',
+  'Popover',
+  'Tooltip',
+  'Tour',
+];
+
+const defaultNode = () => (
+  <>
+    {Object.keys(antd)
+      .filter(name => !blackList.includes(name) && name[0] === name[0].toUpperCase())
+      .map(compName => {
+        const Comp = antd[compName];
+        if (compName === 'Dropdown' || compName === 'Anchor') {
+          return (
+            <Comp key={compName} menu={{ items: [] }}>
+              <div />
+            </Comp>
+          );
+        }
+        return <Comp key={compName} />;
+      })}
+  </>
+);
+
+export function extractStyle(customTheme?: CustomRender): string {
+  const cache = createCache();
+  renderToString(
+    <StyleProvider cache={cache}>
+      {customTheme ? customTheme(defaultNode()) : defaultNode()}
+    </StyleProvider>,
+  );
+
+  // Grab style from cache
+  const styleText = extStyle(cache, true);
+
+  return styleText;
+}
---
+++ components/_util/static-style-extract/interface.ts
@@ -0,0 +1,3 @@
+import type { VueNode } from '../type';
+
+export type CustomRender = (node: VueNode) => VueNode;

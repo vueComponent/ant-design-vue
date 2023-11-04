53dfff048e1bdc5a1bb53624c416342b6941437b
lai9fox <1591765125@qq.com>
2023-11-05 00:36:08 +0800

    docs(tree-select): fix duplicate values in demo

--- components/tree-select/demo/basic.vue
+++ components/tree-select/demo/basic.vue
@@ -1,3 +1,21 @@
+<docs>
+---
+order: 0
+title:
+  zh-CN: 基本用法
+  en-US: Basic usage
+---
+
+## zh-CN
+
+最简单的用法。
+
+## en-US
+
+The most basic usage.
+
+</docs>
+
 <template>
   <a-tree-select
     v-model:value="value"

999e1efdcf76694a34bfae58bd5346818a0c37bd
heart <7362469@qq.com>
2024-01-16 21:20:35 +0800

    fix: fix headerCell slots

--- components/table/hooks/useColumns.tsx
+++ components/table/hooks/useColumns.tsx
@@ -1,10 +1,10 @@
 import devWarning from '../../vc-util/devWarning';
-import { renderSlot } from 'vue';
-import type { Ref } from 'vue';
+import type { VNode, Ref } from 'vue';
 import type { ContextSlots } from '../context';
 import type { TransformColumns, ColumnsType } from '../interface';
 import { SELECTION_COLUMN } from './useSelection';
 import { EXPAND_COLUMN } from '../../vc-table';
+import { customRenderSlot } from 'ant-design-vue/es/_util/vnode';
 
 function fillSlots<RecordType>(columns: ColumnsType<RecordType>, contextSlots: Ref<ContextSlots>) {
   const $slots = contextSlots.value;
@@ -27,7 +27,7 @@ function fillSlots<RecordType>(columns: ColumnsType<RecordType>, contextSlots: R
     });
 
     if (contextSlots.value.headerCell && !column.slots?.title) {
-      cloneColumn.title = renderSlot(
+      cloneColumn.title = customRenderSlot(
         contextSlots.value,
         'headerCell',
         {
@@ -35,7 +35,7 @@ function fillSlots<RecordType>(columns: ColumnsType<RecordType>, contextSlots: R
           column,
         },
         () => [column.title as any],
-      );
+      ) as VNode[];
     }
     if ('children' in cloneColumn && Array.isArray(cloneColumn.children)) {
       cloneColumn.children = fillSlots(cloneColumn.children, contextSlots);

41772ba6f5365862f6b775933bb9c235b938fe16
Carl Chen <ccheartszzz@gmail.com>
2024-06-23 18:29:42 +0800

    fix: adapt to changes in vue3.4+

--- components/_util/eagerComputed.ts
+++ components/_util/eagerComputed.ts
@@ -1,7 +1,14 @@
-import { watchEffect, shallowRef } from 'vue';
+import { watchEffect, shallowRef, version, computed } from 'vue';
 import type { ComputedRef } from 'vue';
 export declare type ComputedGetter<T> = (...args: any[]) => T;
 export default function eagerComputed<T>(fn: ComputedGetter<T>) {
+  const currentVueVersion = Number(version.split('.').slice(0, 2).join(''));
+
+  // version >= 3.4.x, use computed instead of watchSyncEffect
+  if (currentVueVersion >= 34) {
+    return computed(fn);
+  }
+
   const result = shallowRef<T>();
   watchEffect(
     () => {

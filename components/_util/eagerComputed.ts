e4be838da184472744f3d2b7bcd31c42b184d50a
hexi <1317205072@qq.com>
2024-08-09 16:25:24 +0800

    fix(_utils): Compatible with Vue3.4.0's computed breaking changes.

--- components/_util/eagerComputed.ts
+++ components/_util/eagerComputed.ts
@@ -1,16 +1,25 @@
-import { watchEffect, shallowRef } from 'vue';
+import compareVersions from 'compare-versions';
+import { watchEffect, shallowRef, computed, version } from 'vue';
 import type { ComputedRef } from 'vue';
 export declare type ComputedGetter<T> = (...args: any[]) => T;
-export default function eagerComputed<T>(fn: ComputedGetter<T>) {
-  const result = shallowRef<T>();
-  watchEffect(
-    () => {
-      result.value = fn();
-    },
-    {
-      flush: 'sync', // needed so updates are immediate.
-    },
-  );
 
-  return result as any as ComputedRef<T>;
-}
+const breakingChangeVersion = '3.4.0';
+const needEager = compareVersions(version, breakingChangeVersion) < 0;
+
+const eagerComputed = needEager
+  ? function <T>(fn: ComputedGetter<T>) {
+      const result = shallowRef<T>();
+      watchEffect(
+        () => {
+          result.value = fn();
+        },
+        {
+          flush: 'sync', // needed so updates are immediate.
+        },
+      );
+
+      return result as any as ComputedRef<T>;
+    }
+  : computed;
+
+export default eagerComputed;
